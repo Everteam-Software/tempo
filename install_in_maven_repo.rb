267f819c97391fe1ca7a407867e9972d3351a246
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2007-10-10 19:01:23 +0000

    discover and sync maven repositories for given group and version implemented
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@20 f54485bd-ff3a-0410-97a1-83db373c2bce

--- install_in_maven_repo.rb
+++ install_in_maven_repo.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 require "rubygems"
 require "buildr"
+require "hpricot"
+require "open-uri"
+require "pp"
 
+# Some recent code needed to create an artifact from a file 
+# not in current 
 module Buildr
   class Artifact
     def from(path)
@@ -21,18 +26,99 @@ module Buildr
     end 
   end
 
-  if ARGV.length < 2
-    puts "Usage: install_in_maven_repo <spec> <filepath>"
+  def usage
+    puts "Usage1:"
+    puts "  install_in_maven_repo <spec> <filepath>"
+    puts "Install a single file in the current local and remote repositories."
     puts "This also generate the related pom file."
-  else
-    artifact = artifact(ARGV[0]).from(ARGV[1])
+    puts
+    puts "Usage2:"
+    puts "  install_in_maven_repo <group> <version> <remote_url_source> <remote_url_dest>"
+    puts "Copy a group of dependencies from one repository to another"
+  end
+
+  def sync_repositories
+    group_name = ARGV[0]
+    group_version = ARGV[1]
+    url_source = ARGV[2]
+    url_dest = ARGV[3]
+    
+    repositories.remote << url_source
+
+    artifacts = list_artifacts(url_source, group_name)
+    artifacts_d = Array.new
+    artifacts.each do |artifact|
+      artifact_spec = artifact_spec_from_name group_name, group_version, artifact
+      begin
+        a = artifact(artifact_spec)
+        a.invoke
+        artifacts_d << a
+      rescue 
+        puts "Could not download:" + artifact_spec
+      end
+    end
+    
+    artifacts_d.each do |e|
+      install_single_jar(url_dest, e)
+    end
+  end
+  
+  def artifact_spec_from_name group, version, name
+    group + ":" + name + ":jar:" + version
+  end
+  
+  def list_artifacts url_source, group_name
+    r1 = Regexp.new('^[a-z]+')
+    artifacts = Array.new
+
+    sub_url = group_name.gsub(".", '/')
+    url = url_source + "/" + sub_url
+    
+    puts "Accessing:" + url
+    
+    doc = Hpricot(open(url))
+     doc.search("//a").each do |e|
+       if r1.match e.attributes['href'] # skip those not starting with a letter (not artifacts)
+         name = e.inner_html 
+         artifacts << name[0..name.length-2] 
+       end
+     end
+     
+    artifacts
+  end
+  
+  def install_single_artifact repository, spec, from_file
+    artifact = artifact(spec).from(from_file)
     puts "Installing #{ARGV[1]} to #{artifact.name}"
     artifact.invoke
-    repository = "sftp://www.intalio.org/tmp"
+    install_single_jar repository, artifact   
+  end
+  
+  def install_single_jar repository, artifact
+    install_item repository, artifact
+    install_item repository, artifact.pom()
+  end
+  
+  def install_item repository, artifact
     path = artifact.group.gsub(".", "/") + "/#{artifact.id}/#{artifact.version}/#{File.basename(artifact.name)}"
     uri = URI repository + "/" + path
     uri.user = ENV['USER'] if ENV['USER']
     uri.password = ENV['PASSWORD'] if ENV['PASSWORD']
     puts "Uploading #{file(artifact.name)} to #{uri}"
     uri.upload file(artifact.name)
+  end
+
+  # Run script for installing a single artifact  
+  if ARGV.length == 2
+    repository = "sftp://www.intalio.org/tmp"
+    install_single_artifact repository, ARGV[0], ARGV[1]
+  
+  # Copy all artifacts of the specified group for the specified version 
+  # from the source repository (http) to the destination (sftp)   
+  elsif ARGV.length == 4
+    sync_repositories
+  
+  # output usage  
+  else
+      usage  
   end
