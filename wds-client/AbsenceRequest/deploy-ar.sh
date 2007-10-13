7e908aaa9440d60bca8c00cdfa88c1f5ba954de9
boisvert <boisvert@f54485bd-ff3a-0410-97a1-83db373c2bce>
2007-10-13 00:53:44 +0000

    Replace shell script with ruby script
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@36 f54485bd-ff3a-0410-97a1-83db373c2bce

--- wds-client/AbsenceRequest/deploy-ar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-BASE=http://localhost:8080/wds/
-CP=../target/intalio-bpms-workflow-wds-cli-*.jar
-WDSCC=org.intalio.tempo.workflow.wds.cli.WDSCommandLineClient
-
-java -cp $CP $WDSCC -w $BASE store-activity AbsenceRequest/AbsenceApproval.xform AbsenceApproval.xform --force
-java -cp $CP $WDSCC -d ar-deploy.xml -w $BASE store-pipa AbsenceRequest/AbsenceRequest.xform AbsenceRequest.xform --force
-java -cp $CP $WDSCC -w $BASE store-activity AbsenceRequest/Notification.xform Notification.xform --force
