02d1417f17af1a0dabca040c08b4bf1ec923f6cd
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-02-13 18:01:32 +0000

    - formatting - jpa loggers non verbose - edited svn:ignore on deploy* folders
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@274 f54485bd-ff3a-0410-97a1-83db373c2bce

--- tms-common/src/main/java/org/intalio/tempo/workflow/task/Task.java
+++ tms-common/src/main/java/org/intalio/tempo/workflow/task/Task.java
@@ -43,226 +43,222 @@ import org.intalio.tempo.workflow.util.RequiredArgumentException;
 @Entity
 @Table(name = "TASKS")
 @Inheritance(strategy = InheritanceType.JOINED)
-@NamedQueries( { 
-    @NamedQuery(name = Task.FIND_BY_ID, query = "select m from Task m where m._id=?1", hints = { @QueryHint(name = "openjpa.hint.OptimizeResultCount", value = "1") })
-}
-)
-    
+@NamedQueries( { @NamedQuery(name = Task.FIND_BY_ID, query = "select m from Task m where m._id=?1", hints = { @QueryHint(name = "openjpa.hint.OptimizeResultCount", value = "1") }) })
 public abstract class Task extends BaseRestrictedEntity {
 
-	public static final String FIND_BY_ID = "find_by_id";
-	
-	public static final String FIND_BY_IDS = "select m from Task m where m._id IN ";
-	
-	public static final String FIND_BY_USER = "find_by_user";
-	public static final String _FIND_BY_USERS = "find_by_user";
-	public static final String _FIND_BY_ROLES = "find_by_roles";
-	public static final String _FIND_BY_USER_AND_ROLES = "find_by_user_and_roles";
+    public static final String FIND_BY_ID = "find_by_id";
 
-	public static final String FIND_BY_USERS = "SELECT * FROM TASKS m WHERE m.USERS IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})";
-	public static final String FIND_BY_ROLES = "SELECT * FROM TASKS m WHERE m.ROLES IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})";
-	
-	public static final String FIND_BY_USER_AND_ROLES = "SELECT tid from TASKS m WHERE (m.ROLES IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})) OR (m.USERS IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {1}))";
-	
-	@Column(name = "internal_id")
-	@Basic
-	private int _internalId;
+    public static final String FIND_BY_IDS = "select m from Task m where m._id IN ";
 
-	@Column(name = "tid")
-	@Basic
-	private String _id;
+    public static final String FIND_BY_USER = "find_by_user";
+    public static final String _FIND_BY_USERS = "find_by_user";
+    public static final String _FIND_BY_ROLES = "find_by_roles";
+    public static final String _FIND_BY_USER_AND_ROLES = "find_by_user_and_roles";
 
-	@Column(name = "description")
-	@Basic
-	private String _description = "";
+    public static final String FIND_BY_USERS = "SELECT * FROM TASKS m WHERE m.USERS IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})";
+    public static final String FIND_BY_ROLES = "SELECT * FROM TASKS m WHERE m.ROLES IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})";
 
-	@Column(name = "creation_date")
-	@Basic
-	private Date _creationDate = new Date();
+    public static final String FIND_BY_USER_AND_ROLES = "SELECT tid from TASKS m WHERE (m.ROLES IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {0})) OR (m.USERS IN (SELECT SET_ID FROM BACKING_SET WHERE AUTH_ID IN  {1}))";
 
-	@Basic
-	@Column(name = "form_url")
-	private String _formURL;
+    @Column(name = "internal_id")
+    @Basic
+    private int _internalId;
 
-	@PersistentMap(keyCascade = CascadeType.ALL, elementCascade = CascadeType.ALL)
-	@MapKey(name = "action")
-	private Map<String, ACL> _actionACLs = new HashMap<String, ACL>();
+    @Column(name = "tid")
+    @Basic
+    private String _id;
 
-	public Task() {
+    @Column(name = "description")
+    @Basic
+    private String _description = "";
 
-	}
+    @Column(name = "creation_date")
+    @Basic
+    private Date _creationDate = new Date();
 
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof Task))
-			return false;
-		Task t = (Task) o;
-		boolean b = _id.equalsIgnoreCase(t.getID());
-		b &= _formURL.equals(t._formURL);
-		b &= _description.equals(t._description);
-		b &= _creationDate.equals(t._creationDate);
-		b &= bothNullOrEqual(_actionACLs,t._actionACLs);
-		b &= _userOwners.equals(t._userOwners);
-		b &= _roleOwners.equals(_roleOwners);
-		return b;
-	}
+    @Basic
+    @Column(name = "form_url")
+    private String _formURL;
 
-	boolean bothNullOrEqual(Object o1, Object o2) {
-		return o1 == null && o2 == null || o1.equals(o2);
-	}
+    @PersistentMap(keyCascade = CascadeType.ALL, elementCascade = CascadeType.ALL)
+    @MapKey(name = "action")
+    private Map<String, ACL> _actionACLs = new HashMap<String, ACL>();
 
-	public Task(String id, URI formURL) {
-		this.setID(id);
-		this.setFormURL(formURL);
-	}
+    public Task() {
 
-	public Task(String id, String formURL) {
-		this.setID(id);
-		this.setFormURLFromString(formURL);
-	}
+    }
 
-	public boolean equalsTask(Task rhs) {
-		if (rhs == null) {
-			return false;
-		}
-		return _id.equals(rhs._id) && _description.equals(rhs._description);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Task))
+            return false;
+        Task t = (Task) o;
+        boolean b = _id.equalsIgnoreCase(t.getID());
+        b &= _formURL.equals(t._formURL);
+        b &= _description.equals(t._description);
+        b &= _creationDate.equals(t._creationDate);
+        b &= bothNullOrEqual(_actionACLs, t._actionACLs);
+        b &= _userOwners.equals(t._userOwners);
+        b &= _roleOwners.equals(_roleOwners);
+        return b;
+    }
 
-	public String getID() {
-		return _id;
-	}
+    boolean bothNullOrEqual(Object o1, Object o2) {
+        return o1 == null && o2 == null || o1.equals(o2);
+    }
 
-	private void setID(String id) {
-		if (id == null) {
-			throw new RequiredArgumentException("id");
-		}
-		_id = id;
-	}
+    public Task(String id, URI formURL) {
+        this.setID(id);
+        this.setFormURL(formURL);
+    }
 
-	public int getInternalId() {
-		return _internalId;
-	}
+    public Task(String id, String formURL) {
+        this.setID(id);
+        this.setFormURLFromString(formURL);
+    }
 
-	public void setInternalID(int internalId) {
-		_internalId = internalId;
-	}
+    public boolean equalsTask(Task rhs) {
+        if (rhs == null) {
+            return false;
+        }
+        return _id.equals(rhs._id) && _description.equals(rhs._description);
+    }
 
-	public String getDescription() {
-		return _description;
-	}
+    public String getID() {
+        return _id;
+    }
 
-	public void setDescription(String description) {
-		if (description == null) {
-			throw new RequiredArgumentException("description");
-		}
-		_description = description;
-	}
+    private void setID(String id) {
+        if (id == null) {
+            throw new RequiredArgumentException("id");
+        }
+        _id = id;
+    }
 
-	public Date getCreationDate() {
-		return _creationDate;
-	}
+    public int getInternalId() {
+        return _internalId;
+    }
 
-	public void setCreationDate(Date date) {
-		if (date == null) {
-			throw new RequiredArgumentException("date");
-		}
-		_creationDate = date;
-	}
+    public void setInternalID(int internalId) {
+        _internalId = internalId;
+    }
 
-	public URI getFormURL() {
-		return URI.create(_formURL);
-	}
+    public String getDescription() {
+        return _description;
+    }
 
-	public String getFormURLAsString() {
-		return _formURL.toString();
-	}
+    public void setDescription(String description) {
+        if (description == null) {
+            throw new RequiredArgumentException("description");
+        }
+        _description = description;
+    }
 
-	public void setFormURL(URI formURL) {
-		if (formURL == null) {
-			throw new RequiredArgumentException("formURL");
-		}
-		_formURL = formURL.toString();
-	}
+    public Date getCreationDate() {
+        return _creationDate;
+    }
 
-	public void setFormURLFromString(String formURL) {
-		_formURL = convertToFieldURI(formURL).toString();
-	}
+    public void setCreationDate(Date date) {
+        if (date == null) {
+            throw new RequiredArgumentException("date");
+        }
+        _creationDate = date;
+    }
 
-	protected URI convertToFieldURI(String fieldURL) {
-		if (fieldURL == null) {
-			throw new RequiredArgumentException("formURL");
-		}
-		try {
-			return new URI(fieldURL);
-		} catch (Exception e) {
-			throw new InvalidTaskException("Form URL is not valid", e);
-		}
-	}
+    public URI getFormURL() {
+        return URI.create(_formURL);
+    }
 
-	public void authorizeActionForUser(String action, String user) {
-		ACL acl = getACLs(action);
-		acl.users.add(user);
-	}
+    public String getFormURLAsString() {
+        return _formURL.toString();
+    }
 
-	public void authorizeActionForRole(String action, String role) {
-		ACL acl = getACLs(action);
-		acl.roles.add(role);
-	}
+    public void setFormURL(URI formURL) {
+        if (formURL == null) {
+            throw new RequiredArgumentException("formURL");
+        }
+        _formURL = formURL.toString();
+    }
 
-	public void authorizeActionForUsers(String action, AuthIdentifierSet users) {
-		ACL acl = getACLs(action);
-		acl.users.addAll(users);
-	}
+    public void setFormURLFromString(String formURL) {
+        _formURL = convertToFieldURI(formURL).toString();
+    }
 
-	public void authorizeActionForRoles(String action, AuthIdentifierSet roles) {
-		ACL acl = getACLs(action);
-		acl.roles.addAll(roles);
-	}
+    protected URI convertToFieldURI(String fieldURL) {
+        if (fieldURL == null) {
+            throw new RequiredArgumentException("formURL");
+        }
+        try {
+            return new URI(fieldURL);
+        } catch (Exception e) {
+            throw new InvalidTaskException("Form URL is not valid", e);
+        }
+    }
 
-	private ACL getACLs(String action) {
-		ACL acl = _actionACLs.get(action);
-		if (acl == null) {
-			acl = new ACL(action);
-			_actionACLs.put(action, acl);
-		}
-		return acl;
-	}
+    public void authorizeActionForUser(String action, String user) {
+        ACL acl = getACLs(action);
+        acl.users.add(user);
+    }
 
-	public AuthIdentifierSet getAuthorizedUsers(String action) {
-		ACL acl = _actionACLs.get(action);
-		if (acl != null)
-			return acl.users;
-		return new AuthIdentifierSet();
-	}
+    public void authorizeActionForRole(String action, String role) {
+        ACL acl = getACLs(action);
+        acl.roles.add(role);
+    }
 
-	public AuthIdentifierSet getAuthorizedRoles(String action) {
-		ACL acl = _actionACLs.get(action);
-		if (acl != null)
-			return acl.roles;
-		return new AuthIdentifierSet();
-	}
+    public void authorizeActionForUsers(String action, AuthIdentifierSet users) {
+        ACL acl = getACLs(action);
+        acl.users.addAll(users);
+    }
 
-	public boolean isAuthorizedAction(UserRoles user, String action) {
-		// Note: Action is authorized if there's no ACL provided (default)
-		ACL acl = _actionACLs.get(action);
-		if (acl == null)
-			return true;
-		if (acl.users.isEmpty() && acl.roles.isEmpty())
-			return true;
-		if (acl.users.contains(user.getUserID()))
-			return true;
-		if (acl.roles.intersects(user.getAssignedRoles()))
-			return true;
-		return false;
-	}
+    public void authorizeActionForRoles(String action, AuthIdentifierSet roles) {
+        ACL acl = getACLs(action);
+        acl.roles.addAll(roles);
+    }
 
-	public Set<String> getAuthorizedActions() {
-		return _actionACLs.keySet();
-	}
+    private ACL getACLs(String action) {
+        ACL acl = _actionACLs.get(action);
+        if (acl == null) {
+            acl = new ACL(action);
+            _actionACLs.put(action, acl);
+        }
+        return acl;
+    }
 
-	@Override
-	public String toString() {
-		return "Workflow Task " + _id;
+    public AuthIdentifierSet getAuthorizedUsers(String action) {
+        ACL acl = _actionACLs.get(action);
+        if (acl != null)
+            return acl.users;
+        return new AuthIdentifierSet();
+    }
+
+    public AuthIdentifierSet getAuthorizedRoles(String action) {
+        ACL acl = _actionACLs.get(action);
+        if (acl != null)
+            return acl.roles;
+        return new AuthIdentifierSet();
+    }
+
+    public boolean isAuthorizedAction(UserRoles user, String action) {
+        // Note: Action is authorized if there's no ACL provided (default)
+        ACL acl = _actionACLs.get(action);
+        if (acl == null)
+            return true;
+        if (acl.users.isEmpty() && acl.roles.isEmpty())
+            return true;
+        if (acl.users.contains(user.getUserID()))
+            return true;
+        if (acl.roles.intersects(user.getAssignedRoles()))
+            return true;
+        return false;
+    }
+
+    public Set<String> getAuthorizedActions() {
+        return _actionACLs.keySet();
+    }
+
+    @Override
+    public String toString() {
+        return "Workflow Task " + _id;
     }
 
 }
