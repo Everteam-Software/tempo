bd5b48458ee1cac3b2ae629a3ae414428535cedc
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-02-25 02:16:11 +0000

    reorganize imports for scenario to pass properly. also using different connection settings for datasource.
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@291 f54485bd-ff3a-0410-97a1-83db373c2bce

--- wds-service/src/test/stories/jpaStory.groovy
+++ wds-service/src/test/stories/jpaStory.groovy
@@ -11,22 +11,22 @@ import org.intalio.tempo.workflow.wds.core.tms.TMSJPAConnectionFactory
 import org.intalio.tempo.workflow.wds.core.tms.TMSJPAConnection
 import org.intalio.tempo.workflow.wds.core.tms.TMSConnectionInterface
 import org.intalio.tempo.workflow.auth.UserRoles
+import java.net.URI
 
-def convertBundleToProperties(ResourceBundle rb) {
+def getJPAProperties() {
 	props = new Properties()
-	for (Enumeration<String> keys = rb.getKeys(); keys.hasMoreElements();) {
-		String key = (String) keys.nextElement()
-		props.put(key, rb.getString(key))
-	}
+	props.put("openjpa.ConnectionDriverName","org.apache.derby.jdbc.EmbeddedDriver")
+	props.put("openjpa.ConnectionURL","jdbc:derby:wds-service/target/JPADB;create=true")
+	props.put("openjpa.jdbc.SynchronizeMappings","buildSchema")
 	return props
 }
 def getSampleItem() {
 	return new Item("http://www.hellonico.net", "meta", "hello".getBytes())
 }
+
 scenario "storing items using jpa", {
 	given "a JPA Item connection ", {
-		map = convertBundleToProperties(ResourceBundle.getBundle("jpa"))
-		factory = new JPAItemDaoConnectionFactory(map)
+		factory = new JPAItemDaoConnectionFactory(getJPAProperties())
 		jpac = (JPAItemDaoConnection)factory.getItemDaoConnection()  	
 	}
 	when "an item is stored", {
@@ -52,43 +52,15 @@ scenario "storing items using jpa", {
 	}
 }
 
-scenario "storing pipa using jpa", {
-	given "a JPA Pipa connection", {
-		map = convertBundleToProperties(ResourceBundle.getBundle("jpa"))
-		factory = new TMSJPAConnectionFactory(map)
-		jpac = (TMSConnectionInterface)factory.getTMSConnection()
-		formUrl = "http://localhost/"
-	} 
-	when "a PIPA is created", {
-		task1 = new PIPATask("abc",formUrl)
-		task1.formNamespace = "urn:ns"
-		task1.processEndpoint = "http://localhost/process"
-		task1.initOperationSOAPAction = "initProcess"
-	}
-	then "it can be stored", {
-		jpac.storePipaTask(task1)
-	}
-	then "it can be deleted", {
-		jpac.deletePipaTask(formUrl)
-	}
-	and
-	then "trying to delete the same task again throws an exception", {
-		ensureThrows(Exception.class) {
-			jpac.deletePipaTask(formUrl)
-		}
-	}
-}
 scenario "testing list of pipa tasks", {
 	given "a JPA Pipa connection", {
-		map = convertBundleToProperties(ResourceBundle.getBundle("jpa"))
-		factory = new TMSJPAConnectionFactory(map)
+		factory = new TMSJPAConnectionFactory(getJPAProperties())
 		jpac = (TMSJPAConnection) factory.getTMSConnection()
-		formUrl = "http://localhost/"
+		formUrl = "http://localhost/2"
 	}
 	when "a PIPA is stored for the user niko with role engineer", {
-		task1 = new PIPATask("abc",formUrl)
-		task1.formNamespace = "urn:ns"
-		task1.processEndpoint = "http://localhost/process"
+		task1 = new PIPATask("abc1",formUrl)
+		task1.processEndpoint = URI.create("http://localhost/process")
 		task1.initOperationSOAPAction = "initProcess"
 		user ="niko"
 		role ="engineer"
@@ -103,5 +75,33 @@ scenario "testing list of pipa tasks", {
 		list = jpac.fetchAllAvailableTasks(urs)
 		ensure(list.size() >= 1)
 		ensure(list[0] instanceof PIPATask)
+	}
+	then "we delete the pipa task properly", {
+		jpac.deletePipaTask(formUrl)
+	}
+}
+
+scenario "storing pipa using jpa", {
+	given "a JPA Pipa connection", {
+		factory = new TMSJPAConnectionFactory(getJPAProperties())
+		jpac = (TMSConnectionInterface)factory.getTMSConnection()
+		formUrl = "http://localhost/1"
+	} 
+	when "a PIPA is created", {
+		task1 = new PIPATask("abc",formUrl)
+		task1.processEndpoint = URI.create("http://localhost/process")
+		task1.initOperationSOAPAction = "initProcess"
+	}
+	then "it can be stored", {
+		jpac.storePipaTask(task1)
+	}
+	then "it can be deleted", {
+		jpac.deletePipaTask(formUrl)
+	}
+	and
+	then "trying to delete the same task again throws an exception", {
+		ensureThrows(Exception.class) {
+			jpac.deletePipaTask(formUrl)
+		}
 	}
 }
