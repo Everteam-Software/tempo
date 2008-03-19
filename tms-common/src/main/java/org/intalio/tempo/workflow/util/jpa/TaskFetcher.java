0c914dc7823d10af33a7bb35868d139b5ed55c42
boisvert <boisvert@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-03-19 21:15:17 +0000

    Fix issue where string parameters are not escaped correctly
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@416 f54485bd-ff3a-0410-97a1-83db373c2bce

--- tms-common/src/main/java/org/intalio/tempo/workflow/util/jpa/TaskFetcher.java
+++ tms-common/src/main/java/org/intalio/tempo/workflow/util/jpa/TaskFetcher.java
@@ -26,9 +26,9 @@ import org.slf4j.LoggerFactory;
 public class TaskFetcher {
     private static final String FIND_BY_IDS = "select m from Task m where m._id IN ";
 
-    private static final String SUBQUERY_FIND_USER = "m.USERS IN (SELECT SET_ID FROM TEMPO_BACKING_SET WHERE AUTH_ID IN  {0})";
-    private static final String SUBQUERY_FIND_ROLE = "m.ROLES IN (SELECT SET_ID FROM TEMPO_BACKING_SET WHERE AUTH_ID IN  {0})";
     private static final String SUBQUERY_SELECT_FROM_TASKS = "SELECT tid FROM TEMPO_TASKS m WHERE " ; 
+    private static final String SUBQUERY_FIND_USER = "m.USERS IN (SELECT SET_ID FROM TEMPO_BACKING_SET WHERE AUTH_ID IN {0})";
+    private static final String SUBQUERY_FIND_ROLE = "m.ROLES IN (SELECT SET_ID FROM TEMPO_BACKING_SET WHERE AUTH_ID IN {0})";
     private static final String FIND_BY_USERS = SUBQUERY_SELECT_FROM_TASKS+ SUBQUERY_FIND_USER;
     private static final String FIND_BY_ROLES = SUBQUERY_SELECT_FROM_TASKS + SUBQUERY_FIND_ROLE;
 
@@ -61,24 +61,33 @@ public class TaskFetcher {
      * Core method. retrieve all the tasks for the given <code>UserRoles</code>
      */
     public Task[] fetchAllAvailableTasks(UserRoles user) {
+        List<String> params = new ArrayList<String>();
+        params.addAll(user.getAssignedRoles());
+        params.add(user.getUserID());
         String query = SUBQUERY_SELECT_FROM_TASKS+"("
-                + MessageFormat.format(SUBQUERY_FIND_ROLE, user.getAssignedRoles().toString()) + ") OR ("
-                + MessageFormat.format(SUBQUERY_FIND_USER, "('"+user.getUserID() + "')")+")";
-        return fetchTasks(query, null);
+                + MessageFormat.format(SUBQUERY_FIND_ROLE, parameterPlaceholders(user.getAssignedRoles().size()) + ") OR ("
+                + MessageFormat.format(SUBQUERY_FIND_USER, "(??)"))+")";
+        return fetchTasks(query, params);
     }
 
     /**
      * Fetch the tasks for a given user
      */
     public Task[] fetchTasksForUser(String user) {
-        return fetchTasks(FIND_BY_USERS, new Object[] { "('" + user + "')" });
+        List<String> params = new ArrayList<String>();
+        params.add(user);
+        String query = MessageFormat.format(FIND_BY_USERS, "(??)");
+        return fetchTasks(query, params);
     }
 
     /**
      * Fetch the tasks for a given role
      */
     public Object[] fetchTasksForRole(String role) {
-        return fetchTasks(FIND_BY_ROLES, new Object[] { "('" + role + "')" });
+        List<String> params = new ArrayList<String>();
+        params.add(role);
+        String query = MessageFormat.format(FIND_BY_ROLES, "(??)");
+        return fetchTasks(query, params);
     }
 
     /**
@@ -89,22 +98,17 @@ public class TaskFetcher {
         return (List<Task>) q2.getResultList();
     }
 
-    private Task[] fetchTasks(String baseQuery) {
+    private Task[] fetchTasks(String baseQuery, List<String> params) {
         _logger.debug("fetchAllAvailableTasks query:" + baseQuery);
+        baseQuery = renumberParameters(baseQuery);
         Query q = _entityManager.createNativeQuery(baseQuery, String.class);
+        for (int i=0; i<params.size(); i++) 
+            q.setParameter(i+1, params.get(i));
         List<String> listofIds = (List<String>) q.getResultList();
         if (listofIds.size() < 1)
             return new Task[0];
         List<Task> tasks = fetchTaskFromIds(listofIds);
         return (Task[]) new ArrayList(tasks).toArray(new Task[tasks.size()]);
-    }
-
-    /**
-     * Execute the native query, retrieve the ids, and then load the tasks
-     */
-    private Task[] fetchTasks(String baseQuery, Object[] parameters) {
-        String query = (parameters == null) ? baseQuery : MessageFormat.format(baseQuery, parameters);
-        return fetchTasks(query);
     }
 
     /**
@@ -119,6 +123,34 @@ public class TaskFetcher {
         buffer.append(" )");
         _logger.debug("Query:" + buffer.toString());
         return buffer.toString();
+    }
+
+    /**
+     * Generate parameter placeholders for query statement, e.g. (??, ??, ??, ??)
+     */
+    private static String parameterPlaceholders(int params) {
+        StringBuffer buf = new StringBuffer();
+        buf.append("(");
+        for (int i=0; i<params; i++) {
+            buf.append("??,");
+        }
+        buf.deleteCharAt(buf.length() - 1);
+        buf.append(")");
+        return buf.toString();
+    }
+
+    /**
+     * Renumber parameter placeholders, e.g. (??, ??, ??) -> (?1, ?2, ?3) 
+     */
+    private static String renumberParameters(String baseQuery) {
+        int i=1;
+        while (true) {
+            int pos = baseQuery.indexOf("??");
+            if (pos < 0) break;
+            baseQuery = baseQuery.substring(0, pos) + "?" + i + baseQuery.substring(pos+2);
+            i++;
+        }
+        return baseQuery;
     }
 
 }
