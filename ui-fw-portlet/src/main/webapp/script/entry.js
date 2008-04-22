839e5936b85149e668c86cefe627b6b33d914cb2
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-04-22 02:07:10 +0000

    fix bad iframe pop up in portlet
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@499 f54485bd-ff3a-0410-97a1-83db373c2bce

--- ui-fw-portlet/src/main/webapp/script/entry.js
+++ ui-fw-portlet/src/main/webapp/script/entry.js
@@ -5,40 +5,42 @@ var EntryForm = function(){
     // return a public interface
     return {
         init : function(){
-                dialog = new Ext.BasicDialog("entry-dlg", { 
-                        autoTabs:true,
-                        width:850,
-                        height:600,
-                        proxyDrag: true
-                });
-                dialog.addKeyListener(27, dialog.hide, dialog);
-                dialog.addButton('Close', dialog.hide, dialog);
+                // dialog = new Ext.BasicDialog("entry-dlg", { 
+                //                         autoTabs:true,
+                //                         width:600,
+                //                         height:400,
+                //                         proxyDrag: true
+                //                 });
+                //                 dialog.addKeyListener(27, dialog.hide, dialog);
+                //                 dialog.addButton('Close', dialog.hide, dialog);
         },
        
         showDialog : function(url){
             if(!dialog){ // lazy initialize the dialog and only create it once
                 dialog = new Ext.BasicDialog("entry-dlg", { 
-                        autoTabs:true,
-                        width:850,
-                        height:600,
-                        proxyDrag: true
+                        //autoTabs:true,
+                        width:600,
+                        height:380,
+                        proxyDrag: true,
+						shadow: true
                 });
                 dialog.addKeyListener(27, dialog.hide, dialog);
                 dialog.addButton('Close', dialog.hide, dialog);
+				dialog.center
             }
 
             //create iframe
            	var base;
 			var obj;
 			base = document.getElementById("entryUrl");
-			base.innerHTML = "";
 			obj = document.createElement("iframe");
-            //set iframe attribute
 			obj.setAttribute("frameBorder", "0");
-			obj.style.position = "relative";
-			obj.style.width = "800";
-			obj.style.height = "520";
-		
+			//obj.style.position = "relative";
+			obj.style.width = "100%";
+			//obj.style.height = "400%";
+			//obj.setAttribute("height", "400")
+			obj.height="300"
+
 			var tempUrl = url.split("&amp;");
 			var newUrl = tempUrl.join("&");
 			obj.src = newUrl;
