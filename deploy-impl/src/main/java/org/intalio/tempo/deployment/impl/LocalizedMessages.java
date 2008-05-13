5199b78cd74eae8136aa2511a16ef563d6965810
boisvert <boisvert@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-05-13 22:35:33 +0000

    No warning displayed if missing localized bundle; use debug-level instead
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@544 f54485bd-ff3a-0410-97a1-83db373c2bce

--- deploy-impl/src/main/java/org/intalio/tempo/deployment/impl/LocalizedMessages.java
+++ deploy-impl/src/main/java/org/intalio/tempo/deployment/impl/LocalizedMessages.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2005-2007 Intalio inc.
+ * Copyright (c) 2005-2008 Intalio inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,8 @@
 package org.intalio.tempo.deployment.impl;
 
 import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
@@ -28,6 +30,12 @@ public class LocalizedMessages {
     static {
         try {
             _messages = ResourceBundle.getBundle("DeploymentService");
+        } catch (MissingResourceException except) {
+            Locale locale = Locale.getDefault();
+            if (!locale.getLanguage().equals(Locale.US.getLanguage())) {
+                LOG.debug("No available localized message bundle for locale '" + locale + "'; using English language defaults");
+            }
+            // no warning issued for English-based locales
         } catch (Exception except) {
             LOG.warn("Exception while initializing localized message bundle: " + except.toString());
         }
