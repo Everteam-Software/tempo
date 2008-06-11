c6d6e7deb14726e2ff62f99546e4fd6a263e3bda
ark <ark@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-06-11 15:54:10 +0000

    make logout url configurable for CASRBACFilter
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@587 f54485bd-ff3a-0410-97a1-83db373c2bce

--- ui-fw/src/main/java/org/intalio/tempo/uiframework/filter/CASRBACFilter.java
+++ ui-fw/src/main/java/org/intalio/tempo/uiframework/filter/CASRBACFilter.java
@@ -35,7 +35,7 @@ import edu.yale.its.tp.cas.proxy.ProxyTicketReceptor;
 /**
  * @author Arthur
  */
-public class CASRBACFilter implements Filter{
+public class CASRBACFilter implements Filter {
     private static final Logger LOG = LogManager.getLogger("tempo.security");
 
     private static final String TEMPO_CONFIG_DIRECTORY = "org.intalio.tempo.configDirectory";
@@ -44,51 +44,50 @@ public class CASRBACFilter implements Filter{
     private static final String TOKEN_SERVICE = "tokenService";
 
     private static final String CAS_RECEIPT = "edu.yale.its.tp.cas.client.filter.receipt";
+    private static final String SERVICE_URL = "edu.yale.its.tp.cas.client.filter.serviceUrl";
+    private static final String LOGOUT_URL = "edu.yale.its.tp.cas.client.filter.logoutUrl";
 
     private static final String APPLICATION_STATE = "applicationState";
 
-    private static final String LOGOUT_PATH         = "/login.htm";
-    private static final String LOGOUT_METHOD       = "post";
-    private static final String LOGOUT_ACTION_NAME  = "actionName";
+    private static final String LOGOUT_PATH = "/login.htm";
+    private static final String LOGOUT_METHOD = "post";
+    private static final String LOGOUT_ACTION_NAME = "actionName";
     private static final String LOGOUT_ACTION_VALUE = "logOut";
 
-    private static final String JSP_FILE_EXT =".jsp";
     private static final String SESSION_APPLICATION_STATE = "APPLICATION_STATE";
 
     private FilterConfig _filterConfig;
-    
-    public void doFilter(ServletRequest request,
-                     ServletResponse response,
-                     FilterChain chain) throws IOException,ServletException {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         LOG.debug(">> CAS Filter");
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpSession session = httpServletRequest.getSession();
-                
-        if( LOGOUT_PATH.equalsIgnoreCase(httpServletRequest.getServletPath())
-                && LOGOUT_METHOD.equalsIgnoreCase(httpServletRequest.getMethod())
-                && LOGOUT_ACTION_VALUE.equalsIgnoreCase(httpServletRequest.getParameter(LOGOUT_ACTION_NAME))) {
 
-            doSignOut(session,(HttpServletResponse)response);
+        if (LOGOUT_PATH.equalsIgnoreCase(httpServletRequest.getServletPath()) && LOGOUT_METHOD.equalsIgnoreCase(httpServletRequest.getMethod())
+                        && LOGOUT_ACTION_VALUE.equalsIgnoreCase(httpServletRequest.getParameter(LOGOUT_ACTION_NAME))) {
+
+            doSignOut(session, (HttpServletResponse) response);
 
         } else {
 
-            if( null == httpServletRequest.getSession().getAttribute(SESSION_APPLICATION_STATE)) {
+            if (null == httpServletRequest.getSession().getAttribute(SESSION_APPLICATION_STATE)) {
                 doSignIn(httpServletRequest, session);
             }
-            chain.doFilter(request,response);
+            chain.doFilter(request, response);
         }
-        
+
     }
 
     private void doSignIn(HttpServletRequest httpServletRequest, HttpSession session) throws RemoteException {
         LOG.info("signing in with CAS....");
-        String serviceURL = _filterConfig.getInitParameter("edu.yale.its.tp.cas.client.filter.serviceUrl");
+        String serviceURL = _filterConfig.getInitParameter(SERVICE_URL);
         String tempoConfigPath = System.getProperty(TEMPO_CONFIG_DIRECTORY);
-        ApplicationContext appContext = new FileSystemXmlApplicationContext((new StringBuilder("/")).append(tempoConfigPath).append("/").append(SECURITY_CONFIG_XML).toString());
+        ApplicationContext appContext = new FileSystemXmlApplicationContext((new StringBuilder("/")).append(tempoConfigPath).append("/").append(
+                        SECURITY_CONFIG_XML).toString());
 
         TokenService tokenService = (TokenService) appContext.getBean(TOKEN_SERVICE);
         String pgtIou = null;
-        CASReceipt CASreceipt = (CASReceipt)session.getAttribute(CAS_RECEIPT);
+        CASReceipt CASreceipt = (CASReceipt) session.getAttribute(CAS_RECEIPT);
         if (CASreceipt != null)
             pgtIou = CASreceipt.getPgtIou();
         if (pgtIou != null) {
@@ -109,9 +108,9 @@ public class CASRBACFilter implements Filter{
                     try {
                         token = tokenService.getTokenFromTicket(proxyTicket, serviceURL);
                     } catch (AuthenticationException e) {
-                        throw new RuntimeException("Could not get token", e);
+                        throw new RuntimeException("Could not get token!", e);
                     } catch (RBACException e) {
-                        throw new RuntimeException("Could not get token", e);
+                        throw new RuntimeException("Could not get token!", e);
                     }
                     String[] grantedRoles = new String[0];
                     User currentUser = authenticate(tokenService, token, grantedRoles);
@@ -119,56 +118,25 @@ public class CASRBACFilter implements Filter{
                 }
                 ApplicationState.setCurrentInstance(httpServletRequest, state);
             } catch (IOException e) {
-                throw new RuntimeException("Could not get the proxy ticket", e);
+                throw new RuntimeException("Could not get the proxy ticket!", e);
             }
         }
-
-        /*
-        if (null != userName && !"".equals(userName)) {
-            try {
-                LOG.info("signing in for user: " + userName);
-                String token = null;
-                token = tokenService.createToken(userName);
-                Property[] props = tokenService.getTokenProperties(token);
-                String name = extractUser(props);
-                String[] roles = extractRoles(props);
-                User user = new User(name, roles, token);
-                ApplicationState state = ApplicationState.getCurrentInstance(httpServletRequest);
-                if (state == null) {
-                    WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(session.getServletContext());
-                    state = (ApplicationState) context.getBean(APPLICATION_STATE);
-                    try {
-                        state = state.getClass().newInstance();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (state.getCurrentUser() == null) {
-                    state.setCurrentUser(user);
-                }
-                ApplicationState.setCurrentInstance(httpServletRequest, state);
-                LOG.info("user has been signed in successfully.");
-            } catch (RBACException e) {
-                e.printStackTrace();
-            } catch (AuthenticationException e) {
-                e.printStackTrace();
-            }
-        }*/
     }
 
     private void doSignOut(HttpSession session, HttpServletResponse httpServletResponse) throws IOException {
         LOG.info("signing out from CAS..");
         session.invalidate();
-        httpServletResponse.sendRedirect("https://localhost:8443/cas/logout");
+        String logoutURL = _filterConfig.getInitParameter(LOGOUT_URL);
+        httpServletResponse.sendRedirect(logoutURL);
         LOG.info("user has been signed out successfully from Intalio");
     }
 
     public void destroy() {
-        
+
     }
 
     public void init(FilterConfig filterConfig) {
-        
+
         _filterConfig = filterConfig;
     }
 
@@ -198,7 +166,7 @@ public class CASRBACFilter implements Filter{
 
         return result;
     }
-    
+
     protected User authenticate(TokenService tokenService, String token, String[] grantedRoles) throws SecurityException {
         try {
             Property[] props = tokenService.getTokenProperties(token);
