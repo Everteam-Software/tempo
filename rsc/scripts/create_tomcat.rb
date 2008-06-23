7a3b7a63b5b8db5631bf8ee914dd3e789e48802c
ark <ark@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-06-23 16:02:47 +0000

    script for creating liferay tomcate 5.5 5.0.1 bundle
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@607 f54485bd-ff3a-0410-97a1-83db373c2bce

--- rsc/scripts/create_tomcat.rb
+++ rsc/scripts/create_tomcat.rb
@@ -11,7 +11,6 @@ require 'fileutils'
 require 'open-uri'
 require "buildr"
 
-
 script_folder = File.dirname(File.expand_path("#{$0}"))
 
 load "#{script_folder}/../build/dependencies.rb"
@@ -23,12 +22,15 @@ config = YAML::load( File.open( "#{script_folder}/config.yml" ) )
 TEMPO_SVN = "#{script_folder}/../.."
 DEBUG = config["debug"]
 REBUILD_TEMPO = config["rebuild"]
+SERVER = config["server"]
+LIFERAY = "liferay_v501"
 APACHE_MIRROR = find_apache_mirror
 TOMCAT_5_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-5/v5.5.26/bin/apache-tomcat-5.5.26.zip"
 TOMCAT_6_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-6/v6.0.16/bin/apache-tomcat-6.0.16.zip"
 TOMCAT_ADMIN_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-5/v5.5.26/bin/apache-tomcat-5.5.26-admin.zip"
 AXIS_DOWNLOAD = APACHE_MIRROR + "ws/axis2/1_3/axis2-1.3-war.zip"
 ODE_DOWNLOAD = "http://www.intalio.org/public/apache-ode-war-1.1.2.3.zip"
+LIFERAY_V501 = "http://internap.dl.sourceforge.net/sourceforge/lportal/liferay-portal-tomcat-5.5-5.0.1.zip"  #CA
 
 title "Changing directory"
 install_dir = config["install_dir"]
@@ -40,17 +42,21 @@ Dir.chdir install_dir
 title "Downloading required files"
 explain "Downloading tomcat (java web application engine), ode (process engine) and axis (web service engine)"
 ##
-tomcat_folder = download_unzip( TOMCAT_5_DOWNLOAD )
+tomcat_folder = download_unzip( TOMCAT_5_DOWNLOAD ) if SERVER != LIFERAY
 ode_folder = download_unzip( ODE_DOWNLOAD )
 axis_folder = download_unzip( AXIS_DOWNLOAD, false)
+liferay_folder = download_unzip( LIFERAY_V501 ) if SERVER == LIFERAY
 unzip2( filename_from_url( AXIS_DOWNLOAD ), axis_folder )
 ##
 
 title "Define install variables"
 explain "Defining variables for webapp folders and services folders"
+server_folder = tomcat_folder
+server_folder = liferay_folder if SERVER == LIFERAY
 ##
 finder = Finder.new
-webapp_folder = tomcat_folder + File::SEPARATOR + "webapps"
+webapp_folder = server_folder + File::SEPARATOR + "webapps"
+
 ode_war = finder.find_war( ode_folder )
 axis_war = finder.find_war( axis_folder )
 ##
@@ -107,7 +113,8 @@ wi.install_tempo_war( "ui-fw" )
 wi.install_tempo_war( "wds-service", "wds" )
 wi.install_tempo_war( "xforms-manager", "xFormsManager" )
 wi.install_tempo_war( "tms-feeds", "feeds")
-# wi.install_tempo_war( "cas-server-webapp", "cas" )
+#wi.install_tempo_war( "cas-server-webapp", "cas" )
+wi.install( "#{TEMPO_SVN}/rsc/liferay501/cas-web.war", "cas.war")
 # wi.install_tempo_war( "ui-pluto", "pluto" )
 # wi.install_tempo_war( "ui-fw-portlet")
 ##
@@ -116,7 +123,7 @@ title "Install xpath extension in Ode"
 explain "Required library for Ode"
 ##
 ode_webinf = File.expand_path("#{ode_war_folder}/WEB-INF")
-ode_processes_folder = "#{tomcat_folder}/var/processes"
+ode_processes_folder = "#{server_folder}/var/processes"
 processes_folder = "#{TEMPO_SVN}/processes/"
 opi = OdeProcessInstaller.new ode_processes_folder, processes_folder
 xp_jar = finder.search( "#{processes_folder}/xpath-extensions/target", "jar")
@@ -139,8 +146,8 @@ opi.install_process_from_tempo_trunk "AbsenceRequest"
 title "Installing missing libs into Tomcat"
 explain "Some libs are missing in tomcat, so we add them here."
 ##
-lib_folder = "#{tomcat_folder}/common/lib" # tomcat5
-#lib_folder = "#{tomcat_folder}/lib" # tomcat6
+lib_folder = "#{server_folder}/common/lib" # tomcat5
+#lib_folder = "#{server_folder}/lib" # tomcat6
 FileUtils.mkdir_p lib_folder
 MISSING_LIBS= [
   # Mysql driver
@@ -188,7 +195,7 @@ explain "Tempo has a set of required config files, generally one for each compon
 explain "In this build, we focus on a mysql datasource, so we define a JNDI DS in the tomcat config files"
 explain "Also copying the ode config files"
 ##
-tomcat_config_folder = "#{tomcat_folder}/var/config"
+tomcat_config_folder = "#{server_folder}/var/config"
 FileUtils.mkdir_p tomcat_config_folder
 tempo_svn_config_folder = "#{TEMPO_SVN}/config"
 config_files = File.join(tempo_svn_config_folder,"*.*")
@@ -198,21 +205,30 @@ mysql_ds_config_files = File.join("#{TEMPO_SVN}/rsc/tempo-sql","*.xml")
 Dir.glob(mysql_ds_config_files) {|x| File.copy(x, tomcat_config_folder, DEBUG)}
 
 File.copy "#{TEMPO_SVN}/rsc/tomcat/ode-axis2.properties", tomcat_config_folder
+File.copy "#{TEMPO_SVN}/rsc/liferay501/tempokeystore", tomcat_config_folder if SERVER == LIFERAY
 ##
 
 title "Creating setenv file (java opts and config)"
 explain "Settings java options, including memory settings"
 ##
-tomcat_bin_folder = tomcat_folder + File::SEPARATOR + "bin" + File::SEPARATOR
+tomcat_bin_folder = server_folder + File::SEPARATOR + "bin" + File::SEPARATOR
 create_mode = File::CREAT|File::TRUNC|File::RDWR
 # script for unix
 file_path = tomcat_bin_folder + "setenv.sh"
 file = File.new file_path,create_mode
-file.puts "export JAVA_OPTS=\"-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms64m -Xmx512m -Dorg.intalio.tempo.configDirectory=$CATALINA_HOME/var/config -Dorg.apache.ode.configDir=$CATALINA_HOME/var/config\""
+if SERVER == LIFERAY
+  file.puts "export JAVA_OPTS=\"-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=$CATALINA_HOME/var/config -Dorg.apache.ode.configDir=$CATALINA_HOME/var/config -Djava.security.auth.login.config=$CATALINA_HOME/conf/jaas.config\""
+else
+  file.puts "export JAVA_OPTS=\"-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=$CATALINA_HOME/var/config -Dorg.apache.ode.configDir=$CATALINA_HOME/var/config\""
+end
 # script for windows
 file_path = tomcat_bin_folder + "setenv.bat"
 file = (File.new file_path,create_mode)
-file.puts "set JAVA_OPTS=-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms64m -Xmx512m -Dorg.intalio.tempo.configDirectory=%CATALINA_HOME%\\var\\config -Dorg.apache.ode.configDir=%CATALINA_HOME%\\var\\config"
+if SERVER == LIFERAY
+  file.puts "set JAVA_OPTS=-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=%CATALINA_HOME%\\var\\config -Dorg.apache.ode.configDir=%CATALINA_HOME%\\var\\config -Djava.security.auth.login.config=$CATALINA_HOME\\conf\\jaas.config"
+else
+  file.puts "set JAVA_OPTS=-XX:MaxPermSize=256m -server -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=%CATALINA_HOME%\\var\\config -Dorg.apache.ode.configDir=%CATALINA_HOME%\\var\\config"
+end
 ##
 
 title "Changing file permissions (shell scripts)"
@@ -226,7 +242,7 @@ title "Creating deploy folder and adding AbsenceRequest"
 explain "Adding the forms needed to run the AbsenceRequest example"
 ##
 ar_zip = finder.find_tempo_component( "forms" + File::SEPARATOR + "AbsenceRequest", "zip")
-deploy_folder = "#{tomcat_folder}/var/deploy/AbsenceRequest/"
+deploy_folder = "#{server_folder}/var/deploy/AbsenceRequest/"
 FileUtils.mkdir_p deploy_folder
 unzip2(ar_zip, deploy_folder, true)
 ##
@@ -234,15 +250,52 @@ unzip2(ar_zip, deploy_folder, true)
 title "Copying tomcat config xml files (JNDI resources)"
 explain "Making the deploy registry and the mysql DS available to all tomcat application"
 ##
-Dir.glob(File.join("#{TEMPO_SVN}/rsc/tomcat", "*.*")) {|x| File.copy(x,"#{tomcat_folder}/conf", DEBUG)}
+Dir.glob(File.join("#{TEMPO_SVN}/rsc/tomcat", "*.*")) {|x| File.copy(x,"#{server_folder}/conf", DEBUG)}
+Dir.glob(File.join("#{TEMPO_SVN}/rsc/liferay501", "server.xml")) {|x| File.copy(x,"#{server_folder}/conf", DEBUG)} if SERVER == LIFERAY
 ##
 
 title "Expanding tomcat classpath"
 explain "Final touch, so the logging is done properly"
 ##
 file_cp = "#{tomcat_bin_folder}/setclasspath.sh"
-File.open(file_cp, File::WRONLY|File::APPEND) {|file| file << "export CLASSPATH=$CLASSPATH:$CATALINA_HOME/conf"}
+File.open(file_cp, File::WRONLY|File::APPEND) {|file| file << "export CLASSPATH=$CLASSPATH:$CATALINA_HOME/conf"} if SERVER != LIFERAY
 ##
+
+## For liferay specific
+if SERVER == LIFERAY
+  title "For liferay 5.0.1 specific configuration"
+  explain "Copy castor to ui-fw for cas authentication"
+  File.cp "#{TEMPO_SVN}/rsc/liferay501/castor-1.0.5.jar", "#{webapp_folder}/ui-fw/WEB-INF/lib"
+  
+  explain "Copy cas server authentication handler class and config"
+  FileUtils.cp_r "#{TEMPO_SVN}/cas-server-webapp/target/classes", "#{webapp_folder}/cas/WEB-INF"
+  File.cp "#{TEMPO_SVN}/rsc/liferay501/deployerConfigContext.xml", "#{webapp_folder}/cas/WEB-INF"
+  Dir.glob(File.join("#{TEMPO_SVN}/security*/target", "*.jar")) {|x| File.cp x, "#{webapp_folder}/cas/WEB-INF/lib"}
+  CAS_MISSING_LIBS= [
+    AXIOM, AXIS2, LOG4J, WS_COMMONS_SCHEMA
+  ]
+  CAS_MISSING_LIBS.each {|lib| 
+    locate_and_copy( lib, "#{webapp_folder}/cas/WEB-INF/lib" )
+  }
+  
+  explain "Config liferay portal to use cas authentication"
+  File.cp "#{TEMPO_SVN}/rsc/liferay501/web.xml", "#{webapp_folder}/ROOT/WEB-INF"
+  File.cp "#{TEMPO_SVN}/rsc/liferay501/forliferay-ticketfilter-1.0.1.jar", "#{webapp_folder}/ROOT/WEB-INF/lib"
+  File.cp "#{TEMPO_SVN}/rsc/liferay501/portal-ext.properties", "#{webapp_folder}/ROOT/WEB-INF/classes"
+  File.cp "#{webapp_folder}/ROOT/WEB-INF/lib/casclient.jar", "#{server_folder}/common/lib" 
+  
+  explain "Deploy the ui-fw-portlet"
+  # deploy the ui-fw-portlet file
+  wi.install_tempo_war( "ui-fw-portlet" )
+  # copy the tld file
+  FileUtils.mkdir_p "#{webapp_folder}/ui-fw-portlet/WEB-INF/tld"
+  File.copy "#{TEMPO_SVN}/rsc/liferay501/liferay-portlet.tld", "#{webapp_folder}/ui-fw-portlet/WEB-INF/tld"
+  # copy the util jars
+  Dir.glob("#{TEMPO_SVN}/rsc/liferay501/util*.jar") {|x| File.copy x, "#{webapp_folder}/ui-fw-portlet/WEB-INF/lib"}
+  # delete some conflict jar files
+  Dir.glob(File.join("#{webapp_folder}/ui-fw-portlet/WEB-INF/lib", "portlet*.jar")) {|x| File.delete x}
+  Dir.glob(File.join("#{webapp_folder}", "**/casclient*.jar")) {|x| File.delete x}
+end
 
 title "Deleting war files from webapp folder"
 explain "Make some space in the webapp folder by removing unused war files"
@@ -261,7 +314,7 @@ Dir.glob(File.join("#{webapp_folder}", "**/slf4j*.jar")) {|x| File.delete x}
 ##
 
 title "Almost done !"
-explain "Now create a mysql database named \"bpms\" with access to user <root> and no password"
+explain "Now \ a mysql database named \"bpms\" with access to user <root> and no password"
 explain "Load the ode schema into mysql from the file #{TEMPO_SVN}/rsc/tempo-sql/ode-mysql.sql"
 explain "Once this is done, start tomcat with the following command:"
 explain "./catalina.sh run"
