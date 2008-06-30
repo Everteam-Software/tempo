b5070ffb7839a1b8808a0e171960b892c42af737
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-06-30 06:43:45 +0000

    - updating ode version
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@635 f54485bd-ff3a-0410-97a1-83db373c2bce

--- rsc/scripts/create_tomcat.rb
+++ rsc/scripts/create_tomcat.rb
@@ -29,7 +29,7 @@ TOMCAT_5_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-5/v5.5.26/bin/apache-tomcat-5
 TOMCAT_6_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-6/v6.0.16/bin/apache-tomcat-6.0.16.zip"
 TOMCAT_ADMIN_DOWNLOAD = APACHE_MIRROR + "tomcat/tomcat-5/v5.5.26/bin/apache-tomcat-5.5.26-admin.zip"
 AXIS_DOWNLOAD = APACHE_MIRROR + "ws/axis2/1_3/axis2-1.3-war.zip"
-ODE_DOWNLOAD = "http://www.intalio.org/public/tempo/apache-ode-war-1.1.2.3.zip"
+ODE_DOWNLOAD = "http://www.intalio.org/public/tempo/apache-ode-war-1.1.2.4.zip"
 LIFERAY_V501 = "http://internap.dl.sourceforge.net/sourceforge/lportal/liferay-portal-tomcat-5.5-5.0.1.zip"  #CA
 
 title "Changing directory"
