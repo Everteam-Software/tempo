8c9d236c7ab021311375a335a0661e17b4f8c741
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-07-28 07:17:45 +0000

    - remove ugly redundant random NS prefix in RBAC service
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@703 f54485bd-ff3a-0410-97a1-83db373c2bce

--- security-ws-common/src/main/java/org/intalio/tempo/security/ws/RBACQueryConstants.java
+++ security-ws-common/src/main/java/org/intalio/tempo/security/ws/RBACQueryConstants.java
@@ -5,49 +5,51 @@ import static org.intalio.tempo.security.ws.Constants.RBACQUERY_NS;
 import javax.xml.namespace.QName;
 
 public class RBACQueryConstants {
+        public static final String RBAC_PREFIX = "rbac";
+    
 		public static final QName GET_USER_PROPERTIES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getUserPropertiesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getUserPropertiesResponse", RBAC_PREFIX);
 	 	public static final QName GET_ROLE_PROPERTIES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getRolePropertiesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getRolePropertiesResponse", RBAC_PREFIX);
 	 	public static final QName ASSIGNED_USERS_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAssignedUsersResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAssignedUsersResponse", RBAC_PREFIX);
 	    public static final QName ASSIGNED_ROLES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAssignedRolesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAssignedRolesResponse", RBAC_PREFIX);
 	    public static final QName ROLE_OPERATIONS_ON_OBJECT_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getRoleOperationsOnObjectResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getRoleOperationsOnObjectResponse", RBAC_PREFIX);
 	    public static final QName USER_OPERATIONS_ON_OBJECT_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getUserOperationsOnObjectResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getUserOperationsOnObjectResponse", RBAC_PREFIX);
 
 	    public static final QName AUTHORIZED_USERS_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAuthorizedUsersResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAuthorizedUsersResponse", RBAC_PREFIX);
 	    public static final QName AUTHORIZED_ROLES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAuthorizedRolesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAuthorizedRolesResponse", RBAC_PREFIX);
 	    public static final QName TOP_ROLES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getTopRolesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getTopRolesResponse", RBAC_PREFIX);
 	    public static final QName ASCENDANT_ROLES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAscendantRolesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getAscendantRolesResponse", RBAC_PREFIX);
 	    public static final QName DESCENDANT_ROLES_RESPONSE =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "getDescendantRolesResponse");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "getDescendantRolesResponse", RBAC_PREFIX);
 	    public static final QName REMOTE_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "RemoteFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "RemoteFault", RBAC_PREFIX);
 	    public static final QName USER_NOT_FOUND_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "UserNotFoundFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "UserNotFoundFault", RBAC_PREFIX);
 	    public static final QName ROLE_NOT_FOUND_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "RoleNotFoundFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "RoleNotFoundFault", RBAC_PREFIX);
 	    public static final QName OBJECT_NOT_FOUND_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "ObjectNotFoundFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "ObjectNotFoundFault", RBAC_PREFIX);
 	    public static final QName RBAC_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "RBACFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "RBACFault", RBAC_PREFIX);
 	    public static final QName ILLEGAL_ARGUMENT_EXCEPTION =
-	        new QName(RBACQUERY_NS.getNamespaceURI(), "IllegalArgumentFault");
+	        new QName(RBACQUERY_NS.getNamespaceURI(), "IllegalArgumentFault", RBAC_PREFIX);
 	    
 	    
-	    public static final QName USER = new QName(RBACQUERY_NS.getNamespaceURI(), "user");
-	    public static final QName ROLE = new QName(RBACQUERY_NS.getNamespaceURI(), "role");
-	    public static final QName PROPERTY = new QName(RBACQUERY_NS.getNamespaceURI(), "property");
-	    public static final QName NAME = new QName(RBACQUERY_NS.getNamespaceURI(), "name");
-	    public static final QName VALUE = new QName(RBACQUERY_NS.getNamespaceURI(), "value");
-	    public static final QName OBJECT = new QName(RBACQUERY_NS.getNamespaceURI(), "object");
-	    public static final QName OPERATION = new QName(RBACQUERY_NS.getNamespaceURI(), "operation");
-	    public static final QName REALM = new QName(RBACQUERY_NS.getNamespaceURI(), "realm");
+	    public static final QName USER = new QName(RBACQUERY_NS.getNamespaceURI(), "user", RBAC_PREFIX);
+	    public static final QName ROLE = new QName(RBACQUERY_NS.getNamespaceURI(), "role", RBAC_PREFIX);
+	    public static final QName PROPERTY = new QName(RBACQUERY_NS.getNamespaceURI(), "property", RBAC_PREFIX);
+	    public static final QName NAME = new QName(RBACQUERY_NS.getNamespaceURI(), "name", RBAC_PREFIX);
+	    public static final QName VALUE = new QName(RBACQUERY_NS.getNamespaceURI(), "value", RBAC_PREFIX);
+	    public static final QName OBJECT = new QName(RBACQUERY_NS.getNamespaceURI(), "object", RBAC_PREFIX);
+	    public static final QName OPERATION = new QName(RBACQUERY_NS.getNamespaceURI(), "operation", RBAC_PREFIX);
+	    public static final QName REALM = new QName(RBACQUERY_NS.getNamespaceURI(), "realm", RBAC_PREFIX);
 }
--- security-ws-common/src/main/java/org/intalio/tempo/security/ws/RBACQueryResponseMarshaller.java
+++ security-ws-common/src/main/java/org/intalio/tempo/security/ws/RBACQueryResponseMarshaller.java
@@ -12,198 +12,164 @@ import org.intalio.tempo.security.rbac.RoleNotFoundException;
 import org.intalio.tempo.security.rbac.UserNotFoundException;
 
 public class RBACQueryResponseMarshaller {
-	public static OMElement getAssignedUsersResponse(String[] users) {
+    public static OMElement getAssignedUsersResponse(String[] users) {
         OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ASSIGNED_USERS_RESPONSE);
-        
+
         for (int i = 0; i < users.length; i++) {
-            OMElement user = OM_FACTORY.createOMElement(RBACQueryConstants.USER);
-
-            
+            OMElement user = OM_FACTORY.createOMElement(RBACQueryConstants.USER, response);
             user.setText(users[i]);
-            response.addChild(user);
+        }
 
-                    }
-        
         return response;
     }
+
     public static OMElement getAssignedRolesResponse(String[] roles) {
         OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ASSIGNED_ROLES_RESPONSE);
-        
         for (int i = 0; i < roles.length; i++) {
-            OMElement role = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE);
-
-            
+            OMElement role = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE, response);
             role.setText(roles[i]);
-            response.addChild(role);
-
-                    }
-        
+        }
         return response;
     }
+
     public static OMElement getRoleOperationsOnObjectResponse(String[] operations) {
-    	OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE_OPERATIONS_ON_OBJECT_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE_OPERATIONS_ON_OBJECT_RESPONSE);
         for (int i = 0; i < operations.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.OPERATION);
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.OPERATION, response);
             operation.setText(operations[i]);
-            response.addChild(operation);
+        }
+        return response;
+    }
 
-                    }
-        
-        return response;}
     public static OMElement getUserOperationsOnObjectResponse(String[] operations) {
-    	OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.USER_OPERATIONS_ON_OBJECT_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.USER_OPERATIONS_ON_OBJECT_RESPONSE);
+
         for (int i = 0; i < operations.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.OPERATION);
-
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.OPERATION, response);
             operation.setText(operations[i]);
-            response.addChild(operation);
+        }
 
-                    }
-        
-        return response;}
+        return response;
+    }
+
     public static OMElement getAuthorizedUsersResponse(String[] users) {
-OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.AUTHORIZED_USERS_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.AUTHORIZED_USERS_RESPONSE);
+
         for (int i = 0; i < users.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.USER);
-
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.USER, response);
             operation.setText(users[i]);
-            response.addChild(operation);
+        }
 
-                    }
-        
         return response;
-	}
+    }
+
     public static OMElement getAuthorizedRolesResponse(String[] roles) {
-OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.AUTHORIZED_ROLES_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.AUTHORIZED_ROLES_RESPONSE);
+
         for (int i = 0; i < roles.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE);
-
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE, response);
             operation.setText(roles[i]);
-            response.addChild(operation);
+        }
 
-                    }
-        
         return response;
-	}
+    }
+
     public static OMElement getTopRolesResponse(String[] roles) {
-    	OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.TOP_ROLES_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.TOP_ROLES_RESPONSE);
+
         for (int i = 0; i < roles.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE);
-
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE, response);
             operation.setText(roles[i]);
-            response.addChild(operation);
+        }
 
-                    }
-        
         return response;
-	}
+    }
+
     public static OMElement getAscendantRolesResponse(String[] roles) {
-OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ASCENDANT_ROLES_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ASCENDANT_ROLES_RESPONSE);
+
         for (int i = 0; i < roles.length; i++) {
-            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE);
-
-            
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE, response);
             operation.setText(roles[i]);
-            response.addChild(operation);
+        }
 
-                    }
-        
         return response;
-	}
+    }
+
     public static OMElement getDescendantRolesResponse(String[] roles) {
-    	OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.DESCENDANT_ROLES_RESPONSE);
-    	        
-    	        for (int i = 0; i < roles.length; i++) {
-    	            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE);
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.DESCENDANT_ROLES_RESPONSE);
 
-    	            
-    	            operation.setText(roles[i]);
-    	            response.addChild(operation);
+        for (int i = 0; i < roles.length; i++) {
+            OMElement operation = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE, response);
+            operation.setText(roles[i]);
+        }
 
-    	                    }
-    	        
-    	        return response;
-    		}
+        return response;
+    }
+
     public static OMElement getUserPropertiesResponse(Property[] properties) {
-   	 OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.GET_USER_PROPERTIES_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.GET_USER_PROPERTIES_RESPONSE);
+
         for (int i = 0; i < properties.length; i++) {
-            OMElement property = OM_FACTORY.createOMElement(RBACQueryConstants.PROPERTY);
-            OMElement value = OM_FACTORY.createOMElement(RBACQueryConstants.VALUE);
-            OMElement name = OM_FACTORY.createOMElement(RBACQueryConstants.NAME);
+            OMElement property = OM_FACTORY.createOMElement(RBACQueryConstants.PROPERTY, response);
+            OMElement value = OM_FACTORY.createOMElement(RBACQueryConstants.VALUE, property);
+            OMElement name = OM_FACTORY.createOMElement(RBACQueryConstants.NAME, property);
             value.setText((String) properties[i].getValue());
             name.setText((String) properties[i].getName());
-            property.addChild(name);
-            property.addChild(value);
-            
-            response.addChild(property);
+        }
 
-                    }
-        
         return response;
-	}
+    }
+
     public static OMElement getRolePropertiesResponse(Property[] properties) {
-   	 OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.GET_ROLE_PROPERTIES_RESPONSE);
-        
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.GET_ROLE_PROPERTIES_RESPONSE);
+
         for (int i = 0; i < properties.length; i++) {
-            OMElement property = OM_FACTORY.createOMElement(RBACQueryConstants.PROPERTY);
-            OMElement value = OM_FACTORY.createOMElement(RBACQueryConstants.VALUE);
-            OMElement name = OM_FACTORY.createOMElement(RBACQueryConstants.NAME);
+            OMElement property = OM_FACTORY.createOMElement(RBACQueryConstants.PROPERTY, response);
+            OMElement value = OM_FACTORY.createOMElement(RBACQueryConstants.VALUE, property);
+            OMElement name = OM_FACTORY.createOMElement(RBACQueryConstants.NAME, property);
             value.setText((String) properties[i].getValue());
             name.setText((String) properties[i].getName());
-            property.addChild(name);
-            property.addChild(value);
-            
             response.addChild(property);
+        }
 
-                    }
-        
         return response;
-	}
-	public static OMElement getRBACExceptionResponse(RBACException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.RBAC_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-	}
-	public static OMElement getRemoteExceptionResponse(RemoteException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.REMOTE_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-		
-	}
-	public static OMElement getUserNotFoundExceptionResponse(
-			UserNotFoundException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.USER_NOT_FOUND_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-	}
-	public static OMElement getRoleNotFoundExceptionResponse(
-			RoleNotFoundException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE_NOT_FOUND_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-	}
-	public static OMElement getObjectNotFoundExceptionResponse(
-			ObjectNotFoundException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.OBJECT_NOT_FOUND_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-	}
-	public static OMElement getIllegalArgumentException(
-			IllegalArgumentException e) {
-		OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ILLEGAL_ARGUMENT_EXCEPTION);
-		response.setText(e.getMessage());
-		return response;
-	}
+    }
+
+    public static OMElement getRBACExceptionResponse(RBACException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.RBAC_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+    }
+
+    public static OMElement getRemoteExceptionResponse(RemoteException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.REMOTE_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+
+    }
+
+    public static OMElement getUserNotFoundExceptionResponse(UserNotFoundException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.USER_NOT_FOUND_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+    }
+
+    public static OMElement getRoleNotFoundExceptionResponse(RoleNotFoundException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ROLE_NOT_FOUND_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+    }
+
+    public static OMElement getObjectNotFoundExceptionResponse(ObjectNotFoundException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.OBJECT_NOT_FOUND_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+    }
+
+    public static OMElement getIllegalArgumentException(IllegalArgumentException e) {
+        OMElement response = OM_FACTORY.createOMElement(RBACQueryConstants.ILLEGAL_ARGUMENT_EXCEPTION);
+        response.setText(e.getMessage());
+        return response;
+    }
 }
