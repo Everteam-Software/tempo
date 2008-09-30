c2728a0e8b85b2274bba9c2d16f73c1365e39f56
jackie <jackie@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-09-30 20:29:28 +0000

    unit test code for LoggingInterceptor.java PIPAComponentManager.java PIPALoader.java
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/branches/5.3@902 f54485bd-ff3a-0410-97a1-83db373c2bce

---
+++ tms-service/src/test/java/org/intalio/tempo/workflow/tms/server/PIPAComponentManagerTest.java
@@ -0,0 +1,45 @@
+package org.intalio.tempo.workflow.tms.server;
+
+import java.io.InputStream;
+
+import org.intalio.tempo.deployment.DeploymentMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class PIPAComponentManagerTest extends TestCase {
+    private static final Logger logger = LoggerFactory.getLogger(PIPAComponentManagerTest.class);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(PIPAComponentManagerTest.class);
+    }
+
+    public void testCheckPipa() throws Exception {
+        PIPAComponentManager pcm = new PIPAComponentManager(Utils.createTMSServer());
+        InputStream is = this.getClass().getResourceAsStream("/AbsenceRequest.pipa");
+        DeploymentMessage dm = pcm.checkPipa("token2", is, "pipatest");
+        Assert.assertNull(dm);
+    }
+
+    public void testProcessPipa() throws Exception {
+        PIPAComponentManager pcm = new PIPAComponentManager(Utils.createTMSServer());
+        InputStream is = this.getClass().getResourceAsStream("/AbsenceRequest.pipa");
+        DeploymentMessage dm = pcm.processPipa("token2", is, "pipatest");
+        Assert.assertNull(dm);
+    }
+
+    public void testDeploy() throws Exception {
+        // PIPAComponentManager pcm = new
+        // PIPAComponentManager(Utils.createTMSServer());
+        // InputStream is =
+        // this.getClass().getResourceAsStream("/AbsenceRequest.pipa");
+        // DeploymentMessage dm = pcm.deploy(, base)("token2", is, "pipatest");
+        // logger.info(dm.toString());
+        // do nothing
+    }
+}
