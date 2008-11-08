81831b2b134f0806b0b1fa55c6cb0c6adc632a8f
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-11-08 14:53:02 +0000

    The sample now works with more comments, more metadata examples and less noisy namespaces.
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@1135 f54485bd-ff3a-0410-97a1-83db373c2bce

--- tms-client/src/test/java/org/intalio/tempo/workflow/tms/client/dependent_tests/RemoteAbsenceRequestTest.java
+++ tms-client/src/test/java/org/intalio/tempo/workflow/tms/client/dependent_tests/RemoteAbsenceRequestTest.java
@@ -23,15 +23,19 @@ import org.intalio.tempo.security.Property;
 import org.intalio.tempo.security.authentication.AuthenticationConstants;
 import org.intalio.tempo.security.util.PropertyUtils;
 import org.intalio.tempo.security.ws.TokenClient;
+import org.intalio.tempo.workflow.task.Notification;
 import org.intalio.tempo.workflow.task.PATask;
 import org.intalio.tempo.workflow.task.Task;
 import org.intalio.tempo.workflow.task.xml.XmlTooling;
 import org.intalio.tempo.workflow.tms.client.RemoteTMSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
 
+/**
+ * This supports a fully absence request samples through code and soap requests only
+ */
 public class RemoteAbsenceRequestTest extends TestCase {
+	
 	static final Logger _log = LoggerFactory.getLogger(RemoteAbsenceRequestTest.class);
 
 	private static final String TOKEN_SERVICE = "http://localhost:8080/axis2/services/TokenService";
@@ -39,7 +43,7 @@ public class RemoteAbsenceRequestTest extends TestCase {
 	private static final String TASK_MANAGEMENT_PROCESS = "http://localhost:8080/ode/processes/completeTask";
 	final XmlTooling xmlTooling = new XmlTooling();
 	
-	public void testAll() throws Exception {
+	public void testAbsenceRequest() throws Exception {
 		_log.info("Instanciate token service client");
 		TokenClient client = new TokenClient(TOKEN_SERVICE);
 
@@ -61,26 +65,31 @@ public class RemoteAbsenceRequestTest extends TestCase {
 		
 		String pipaID = ts[0].getID();
 		_log.info("We have found the task to instanciate the process. This task has the following ID:"+pipaID);
-		Document response = tms.init(pipaID, Utils.createXMLDocument("/initPipa.xml"));
+		tms.init(pipaID, Utils.createXMLDocument("/initPipa.xml"));
 		
 		_log.info("wait for the task to be initiated. Hopefully 2s is enough");
 		Thread.sleep(2000);
 		
 		_log.info("get our full activity task list");
 		Task[] paList = tms.getAvailableTasks("PATask", "ORDER BY T._creationDate DESC");
-		_log.info("get the task we want");
-		Task pa = paList[0];
-		String id = pa.getID();
+		_log.info("get the id of the activity task");
+		String id = paList[0].getID();
 		_log.info("We're about to start using PATask with id:"+id);
 		
 		_log.info("We cannot get input and output of a task on a get task list call (see WSDL)");
-		_log.info("Let's call TMS again to get the full data of this PATask");
+		_log.info("Let's call TMS again to get the full input and output data of this PATask");
 		PATask task = (PATask)tms.getTask(id);
-		_log.info("Checking the task metadata." +
+		
+		_log.info("" +
+				"\nChecking the task metadata..." +
 				"\nThe task has been created on:"+task.getCreationDate() +
 				"\nIt has the following description:"+task.getDescription() +
-				"\nIt is attached to the process with id:"+task.getProcessID());
-		_log.info("Task has the following input:\n"+task.getInputAsXmlString());
+				"\nIt is attached to the process with id:"+task.getProcessID() +
+				"\nIt is attached to the following form:"+task.getFormURLAsString() +
+				"\nIt is in the following state:"+task.getState() +
+				"\nIt has the following input:\n"+task.getInputAsXmlString() + 
+				"\nIt can be assigned to the following roles:"+task.getRoleOwners() +
+				"\nIt can be assigned to the following users:"+task.getUserOwners());
 
 		_log.info("Let's claim the task: no one else can access this task apart from user:"+user);
         sendSoapToTMP(claim(token,id, user), "claimTask");
@@ -93,12 +102,23 @@ public class RemoteAbsenceRequestTest extends TestCase {
         Thread.sleep(2000);
         Task[] ts2 = tms.getAvailableTasks("Notification", "ORDER BY T._creationDate DESC");
         String notificationId = ts2[0].getID();
-        _log.info("Dismiss the notification with id:"+notificationId);
+        _log.info("We want to retrieve some more data on the notification with id:"+notificationId);
+        Notification notification = (Notification) tms.getTask(notificationId);
+        
+        _log.info("The notification has the following:" +
+        		"\nInput:"+xmlTooling.serializeXML(notification.getInput())+
+        		"\nCreation Date:"+notification.getCreationDate() +
+        		"\nAttached Form:"+notification.getFormURLAsString() +
+        		"\nDescription:"+notification.getDescription());
+        
+        _log.info("Dismiss this notification");
 		tms.complete(notificationId);
 	}
 	
+	/**
+	 * Send the SOAP request to TMP.
+	 */
 	private void sendSoapToTMP(StringBuffer request, String soapAction) throws Exception {
-		// options to talk to TMP
 		ServiceClient serviceClient = new ServiceClient();
 		OMFactory factory = OMAbstractFactory.getOMFactory();
 		Options options = new Options();
@@ -108,57 +128,53 @@ public class RemoteAbsenceRequestTest extends TestCase {
         serviceClient.sendReceive(xmlTooling.convertDOMToOM(xmlTooling.parseXML(request.toString()), factory));
 	}
 	
+	/**
+	 * Generate a complete request.
+	 * This also adds some static output to the task. 
+	 */
 	private StringBuffer complete(String token, String taskId) throws Exception {
 		StringBuffer complete = new StringBuffer();
-		complete.append("<b4p:taskMetaData><b4p:taskId>"+taskId+"</b4p:taskId></b4p:taskMetaData>");
-		complete.append("<b4p:participantToken>"+token+"</b4p:participantToken>");
+		complete.append("<b4p:taskMetaData><b4p:taskId>").append(taskId).append("</b4p:taskId></b4p:taskMetaData>");
+		complete.append("<b4p:participantToken>").append(token).append("</b4p:participantToken>");
 		complete.append("<b4p:taskOutput>");
-		complete.append("<output ");
-		complete.append("xmlns=\"http://www.intalio.com/workflow/forms/AbsenceRequest/AbsenceRequest\" ");
-		complete.append("xmlns:f=\"http://orbeon.org/oxf/xml/formatting\" ");
-		complete.append("xmlns:xxforms=\"http://orbeon.org/oxf/xml/xforms\" ");
-		complete.append("xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" ");
-		complete.append("xmlns:fe=\"http://www.intalio.com/workflow/forms/AbsenceRequest/AbsenceRequest\" ");
-		complete.append("taskId=\""+taskId+"\">");
-		// looks like we don't need token and user here, as it is send usually from form manager
-		complete.append("<approved>true</approved>" +
-        		"<comment/>" +
-        		"<contactWhileAway>" +
-        		"<name>Emily Williams</name>" +
-        		"<phone>+1(650)596-1800</phone>" +
-        		"<email>ewilliams@examples.intalio.com</email>" +
-        		"</contactWhileAway>" +
-        		"</output>");
+		
+		complete.append("<output xmlns=\"http://www.intalio.com/workflow/forms/AbsenceRequest/AbsenceRequest\">");
+		complete.append("<approved>true</approved><comment/><contactWhileAway><name>Emily Williams</name><phone>+1(650)596-1800</phone><email>ewilliams@examples.intalio.com</email></contactWhileAway>");
+		complete.append("</output>");
+		
         complete.append("</b4p:taskOutput>");
         return parseTMPRequest(token, taskId, "completeTaskRequest", complete);
 	}
 	
+	/**
+	 * Generate a revoke request
+	 */
 	private StringBuffer revoke(String token, String taskId) throws Exception {
 		StringBuffer revoke = new StringBuffer();
-		revoke.append("<b4p:taskId>"+taskId+"</b4p:taskId>");
-		revoke.append("<b4p:participantToken>"+token+"</b4p:participantToken>");
+		revoke.append("<b4p:taskId>").append(taskId).append("</b4p:taskId>");
+		revoke.append("<b4p:participantToken>").append(token).append("</b4p:participantToken>");
 		return parseTMPRequest(token, taskId, "revokeTaskRequest", revoke);
 	}
 	
+	/**
+	 * Generate a claim request, for the given user
+	 */
 	private StringBuffer claim(String token, String taskId, String user) throws Exception {
 		StringBuffer claim = new StringBuffer();
-		claim.append("<b4p:taskId>"+taskId+"</b4p:taskId>");
-		claim.append("<b4p:claimerUser>"+user+"</b4p:claimerUser>");
-		claim.append("<b4p:participantToken>"+token+"</b4p:participantToken>");
+		claim.append("<b4p:taskId>").append(taskId).append("</b4p:taskId>");
+		claim.append("<b4p:claimerUser>").append(user).append("</b4p:claimerUser>");
+		claim.append("<b4p:participantToken>").append(token).append("</b4p:participantToken>");
 		return parseTMPRequest(token, taskId, "claimTaskRequest", claim);
 	}
 
+	/**
+	 * Template for a generic request to TMP
+	 */
 	private StringBuffer parseTMPRequest(String token, String taskId, String requestType, StringBuffer xml){
 		StringBuffer request = new StringBuffer();
-		request.append("<b4p:");
-		request.append(requestType);
-		request.append(" xmlns:b4p=\"http://www.intalio.com/bpms/workflow/ib4p_20051115\"");
-		request.append(" xmlns:ev=\"http://www.w3.org/2001/xml-events\"");
-	    request.append(" xmlns:oxf=\"http://www.orbeon.com/oxf/processors\"");
-		request.append(" xmlns:p=\"http://www.orbeon.com/oxf/pipeline\"");
-		request.append(" xmlns:xforms=\"http://www.w3.org/2002/xforms\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">");
+		request.append("<b4p:").append(requestType).append(" xmlns:b4p=\"http://www.intalio.com/bpms/workflow/ib4p_20051115\">");
 		request.append(xml);
-		request.append("</b4p:"+requestType+">");
+		request.append("</b4p:").append(requestType).append(">");
 		return request;
 	}
 }
