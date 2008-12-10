1a9001a9dc6ffb9d5e10f3d6dd34a697abc46435
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-12-10 11:40:51 +0000

    added documentation to script and test for liferay
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@1200 f54485bd-ff3a-0410-97a1-83db373c2bce

--- rsc/scripts/create_liferay.rb
+++ rsc/scripts/create_liferay.rb
@@ -23,7 +23,10 @@ DEBUG = config["debug"]
 
 title "Creating Liferay release"
 
-# Create and init folders
+#
+# Create and init folders variables
+#
+title "Create and init folders variables"
 FileUtils.mkdir_p INSTALL_DIR
 Dir.chdir INSTALL_DIR
 server_folder = download_unzip( LIFERAY_5 )
@@ -32,57 +35,81 @@ liferay_config_folder = "#{TEMPO_SVN}/rsc/liferay510/"
 tempo_svn_config_folder = "#{TEMPO_SVN}/config"
 tomcat_config_folder = "#{server_folder}/var/config"
 FileUtils.mkdir_p tomcat_config_folder
-
-File.cp "#{TEMPO_SVN}/rsc/liferay510/web.xml", "#{webapp_folder}/ROOT/WEB-INF"
-Dir.glob("#{TEMPO_SVN}/liferay-ticket-filter/target/*.jar") {|x| File.copy x, "#{webapp_folder}/ROOT/WEB-INF/lib", DEBUG}
-File.cp "#{TEMPO_SVN}/rsc/liferay510/portal-ext.properties", "#{webapp_folder}/ROOT/WEB-INF/classes"
-File.cp "#{TEMPO_SVN}/rsc/liferay510/ROOT.xml", "#{webapp_folder}/../conf/Catalina/localhost/"
-
 tomcat_bin_folder = server_folder + File::SEPARATOR + "bin" + File::SEPARATOR
 create_mode = File::CREAT|File::TRUNC|File::RDWR
-# setenv files
+
+###
+# 
+### 
+title "create setenv files"
+explain "This is needed because we need a keystore for SSL and we need the tempo configuration folder"
 file_path = tomcat_bin_folder + "setenv.sh"
 file = File.new file_path,create_mode
 file.puts "export JAVA_OPTS=\"-XX:MaxPermSize=256m -server -Djavax.net.ssl.trustStore=$CATALINA_HOME/var/config/tempokeystore -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=$CATALINA_HOME/var/config -Dorg.apache.ode.configDir=$CATALINA_HOME/var/config -Djava.security.auth.login.config=$CATALINA_HOME/conf/jaas.config\""
 file_path = tomcat_bin_folder + "setenv.bat"
 file = (File.new file_path,create_mode)
 file.puts "set JAVA_OPTS=-XX:MaxPermSize=256m -server -Djavax.net.ssl.trustStore=%CATALINA_HOME%\\var\\config\\tempokeystore -Dfile.encoding=UTF-8 -Xms128m -Xmx1024m -Dorg.intalio.tempo.configDirectory=%CATALINA_HOME%\\var\\config -Dorg.apache.ode.configDir=%CATALINA_HOME%\\var\\config -Djava.security.auth.login.config=%CATALINA_HOME%\\conf\\jaas.config\""
+Dir.glob("#{server_folder}/bin/*.sh") {|x| File.chmod 0755, x, DEBUG}
 
-# copy server.xml
+###
+#
+###
+title "copy server.xml"
+explain "This is needed because we want to change the ports liferay is running on"
 File.copy "#{liferay_config_folder}/server-liferay-standalone.xml", "#{server_folder}/conf/server.xml", DEBUG
 
-# copy web.xml
+###
+# 
+###
+title "copy web.xml"
+explain "this is needed because we need to configure CAS and the CAS filter properly"
 File.copy "#{liferay_config_folder}/web.xml", "#{webapp_folder}/ROOT/WEB-INF/web.xml"
 
-# copy ROOT.xml
+###
+#
+###
+title "copy ROOT.xml"
+explain "This is needed for database configuration. May be skipped later on"
 File.copy "#{liferay_config_folder}/ROOT-liferay-standalone.xml", "#{server_folder}/conf/Catalina/localhost/ROOT.xml", DEBUG
 
-# add tempo liferay filter
+###
+#
+###
+title "add tempo liferay filter"
+explain "This is needed because we are using CAS proxy"
 Dir.glob("#{TEMPO_SVN}/liferay-ticket-filter/target/*.jar") {|x| File.copy x, "#{webapp_folder}/ROOT/WEB-INF/lib", DEBUG}
 
-# move casclient jar file
+###
+#
+###
+title "move casclient jar file"
+explain "casclient should not be in a single webapp, otherwise it ends up being loaded by the wrong classlooader, and ends up in a ClassCastException"
 begin
 File.mv "#{webapp_folder}/ROOT/WEB-INF/lib/casclient.jar", "#{server_folder}/common/lib"
 rescue
   # must have been moved already, ignore
 end
 
-# copy portal-ext.properties
+###
+#
+###
+title "copy portal-ext.properties"
+explain "This is where CAS and LDAP are configured"
 File.copy "#{liferay_config_folder}/../LDAP/portal-ext.properties", "#{webapp_folder}/ROOT/WEB-INF/classes", DEBUG
 
-# copy files in var/config folder
+###
+#
+###
+title "copy files in var/config folder"
+explain "Those are required tempo files. We load them from the tempo config folder defined in setenv.sh/.bat"
 File.copy "#{tempo_svn_config_folder}/tempo-formmanager.xml", tomcat_config_folder
 File.copy "#{tempo_svn_config_folder}/tempo-ui-fw-servlet.xml", tomcat_config_folder
 File.copy "#{tempo_svn_config_folder}/tempo-ui-fw.xml", tomcat_config_folder
 File.copy "#{liferay_config_folder}/tempokeystore", tomcat_config_folder
 
-# change sh files settings
-Dir.glob("#{server_folder}/bin/*.sh") {|x| File.chmod 0755, x, DEBUG}
-
-# add portlet version of ui-fw
+title "add portlet version of ui-fw"
+explain "This is just the same as the standard, except we replace the web.xml file."
 Dir.chdir "#{TEMPO_SVN}/ui-fw"
 system "buildr -e portlet clean install" if config['rebuild']
 Dir.chdir INSTALL_DIR
-Dir.glob("#{TEMPO_SVN}/ui-fw/target/*.war") {|x| 
-  File.copy x, "#{webapp_folder}/ui-fw.war", DEBUG
-}
+Dir.glob("#{TEMPO_SVN}/ui-fw/target/*.war") {|x|  File.copy x, "#{webapp_folder}/ui-fw.war", DEBUG}
