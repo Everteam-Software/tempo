a88a6f402f036ef9e4142350644d0756ddfed64c
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2008-12-24 06:33:02 +0000

    new maven repo on intalio.org
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@1240 f54485bd-ff3a-0410-97a1-83db373c2bce

--- rsc/build/repositories.rb
+++ rsc/build/repositories.rb
@@ -8,5 +8,5 @@ repositories.remote = [
 ]
 
 repositories.release_to[:username] ||= "release"
-repositories.release_to[:url] ||= "sftp://www.intalio.org/var/www/public/maven2"
+repositories.release_to[:url] ||= "sftp://www.intalio.org/var/www-org/public/maven2"
 repositories.release_to[:permissions] ||= 0664
