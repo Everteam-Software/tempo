f58765f1c44f939357ab5c2598fb4adeff63c719
jackie <jackie@f54485bd-ff3a-0410-97a1-83db373c2bce>
2009-01-05 08:30:47 +0000

    Just put TMS service and dummy implementation
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/branches/b4p@1252 f54485bd-ff3a-0410-97a1-83db373c2bce

---
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSServer.java
@@ -0,0 +1,13 @@
+package org.intalio.tempo.workflow.tmsb4p.server;
+
+public class TMSServer {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
