d7a71db28ae18e6b4aaa22e876580e3dcf55f187
nico <nico@f54485bd-ff3a-0410-97a1-83db373c2bce>
2009-01-09 05:58:20 +0000

    committing changes for compressed token after all. Can be enabled and disabled at will using the <property name=compressToken><value>true</value></property> in securityConfig.xml
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/trunk@1268 f54485bd-ff3a-0410-97a1-83db373c2bce

--- security/src/test/java/org/intalio/tempo/security/simple/SimpleTest.java
+++ security/src/test/java/org/intalio/tempo/security/simple/SimpleTest.java
@@ -85,7 +85,7 @@ public class SimpleTest
 		
 		provider = getProvider();
 		realms = provider.getRealms();
-		assertSameValues( new String[] { "intalio", "exolab", "" },
+		assertSameValues( new String[] { "intalio", "exolab", "proto", "" },
 						  realms );
 						      	
 						      	
