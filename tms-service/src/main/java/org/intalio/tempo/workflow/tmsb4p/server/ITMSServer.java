f166c596924c46e92c66a65b543cc7f3423a7d3c
jackie <jackie@f54485bd-ff3a-0410-97a1-83db373c2bce>
2009-01-20 22:14:31 +0000

    operation getMyTasks
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/branches/b4p@1296 f54485bd-ff3a-0410-97a1-83db373c2bce

---
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/ITMSServer.java
@@ -0,0 +1,5 @@
+package org.intalio.tempo.workflow.tmsb4p.server;
+
+public interface ITMSServer {
+
+}
--- tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSRequestProcessor.java
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSRequestProcessor.java
@@ -4,30 +4,34 @@ import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
 import org.apache.axis2.AxisFault;
-import org.intalio.tempo.deployment.utils.DeploymentServiceRegister;
 import org.intalio.tempo.workflow.auth.AuthException;
-import org.intalio.tempo.workflow.task.xml.TaskXMLConstants;
+import org.intalio.tempo.workflow.task.xml.XmlTooling;
 import org.intalio.tempo.workflow.tms.AccessDeniedException;
 import org.intalio.tempo.workflow.tms.TMSException;
 import org.intalio.tempo.workflow.tms.UnavailableAttachmentException;
 import org.intalio.tempo.workflow.tms.UnavailableTaskException;
-import org.intalio.tempo.workflow.tms.server.PIPAComponentManager;
 import org.intalio.tempo.workflow.tms.server.TMSConstants;
 import org.intalio.tempo.workflow.util.xml.InvalidInputFormatException;
 import org.intalio.tempo.workflow.util.xml.OMMarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TMSRequestProcessor {   
+import com.intalio.wsHT.api.xsd.CreateDocument;
+import com.intalio.wsHT.api.xsd.CreateResponseDocument;
+import com.intalio.wsHT.api.xsd.GetMyTasksDocument;
+import com.intalio.wsHT.api.xsd.GetMyTasksResponseDocument;
+
+public class TMSRequestProcessor {
     final static Logger _logger = LoggerFactory.getLogger(TMSRequestProcessor.class);
     private ITMSServer _server;
     private static final OMFactory OM_FACTORY = OMAbstractFactory.getOMFactory();
-    
+
     /**
      * dumy function
+     * 
      * @return
      */
-    public OMElement marshallResponse(){
+    public OMElement marshallResponse() {
         return new TMSResponseMarshaller(OM_FACTORY) {
             public OMElement createOkResponse() {
                 OMElement okResponse = createElement("okResponse");
@@ -35,8 +39,8 @@ public class TMSRequestProcessor {
             }
         }.createOkResponse();
     }
-    
-    public OMElement createOkResponse(){
+
+    public OMElement createOkResponse() {
         return new TMSResponseMarshaller(OM_FACTORY) {
             public OMElement createOkResponse() {
                 OMElement okResponse = createElement("okResponse");
@@ -44,6 +48,7 @@ public class TMSRequestProcessor {
             }
         }.createOkResponse();
     }
+
     private AxisFault makeFault(Exception e) {
         if (e instanceof TMSException) {
             if (_logger.isDebugEnabled())
@@ -75,26 +80,61 @@ public class TMSRequestProcessor {
             return AxisFault.makeFault(e);
         }
     }
+
     public void setServer(ITMSServer server) {
         _logger.debug("TMSRequestProcessor.setServer:" + server.getClass().getSimpleName());
         _server = server;
     }
-   
-    public OMElement create(OMElement requestElement) throws AxisFault{
-        try{
-        // unmarshall request
-        
-        
-        // process request
-      // _server.create(task, participantToken);
-        
-        // marshall response        
-        return createOkResponse();
-    } catch (Exception e) {
-        throw makeFault(e);
+
+    ///////////////////////////
+    // operations
+    ///////////////////////////
+    public OMElement create(OMElement requestElement) throws AxisFault {
+        try {
+            // unmarshal request
+            CreateDocument req = CreateDocument.Factory.parse(requestElement.getXMLStreamReader());
+            
+            // TODO process request
+            // _server.create(task, participantToken);
+
+            // marshal response
+            CreateResponseDocument ret = CreateResponseDocument.Factory.newInstance();
+            //TODO set data 
+            
+            // convert to OMElment
+            return XmlTooling.convertDocument(ret);
+            //return this.createOkResponse();
+
+        } catch (Exception e) {
+            throw makeFault(e);
+        }
+
     }
+
+    public OMElement getMyTasks(OMElement requestElement) throws AxisFault {
+        try {
+            // unmarshal request
+            GetMyTasksDocument req = GetMyTasksDocument.Factory.parse(requestElement.getXMLStreamReader());
+            
+            // TODO process request
+            // _server.create(task, participantToken);
+
+            // marshal response
+            GetMyTasksResponseDocument ret = GetMyTasksResponseDocument.Factory.newInstance();
+            //TODO set data 
+            
+            // convert to OMElment
+            return XmlTooling.convertDocument(ret);
+            //return this.createOkResponse();
+
+        } catch (Exception e) {
+            throw makeFault(e);
+        }
+
+    }
+  
     
-    }
+    
     /**
      * @param args
      */
@@ -102,9 +142,11 @@ public class TMSRequestProcessor {
         // TODO Auto-generated method stub
 
     }
+
     private abstract class TMSResponseMarshaller extends OMMarshaller {
+        
         public TMSResponseMarshaller(OMFactory omFactory) {
-            super(omFactory, omFactory.createOMNamespace("http://www.intalio.com/BPMS/Workflow/TaskManagementServices-20051109/", "tmsb4p"));
+            super(omFactory, omFactory.createOMNamespace("http://www.intalio.com/BPMS/Workflow/HumanTaskOperationServices-20081209/", "tmsb4p"));
         }
     }
 }
