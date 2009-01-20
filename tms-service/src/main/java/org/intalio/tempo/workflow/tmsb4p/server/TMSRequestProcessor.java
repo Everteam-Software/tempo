78f01d30651cf4f7a19290b385ac7e7ed4c7d62b
jackie <jackie@f54485bd-ff3a-0410-97a1-83db373c2bce>
2009-01-20 02:54:19 +0000

    create first dummy b4p operation "create"
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/branches/b4p@1295 f54485bd-ff3a-0410-97a1-83db373c2bce

--- tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSRequestProcessor.java
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSRequestProcessor.java
@@ -1,7 +1,100 @@
 package org.intalio.tempo.workflow.tmsb4p.server;
 
-public class TMSRequestProcessor {
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axis2.AxisFault;
+import org.intalio.tempo.deployment.utils.DeploymentServiceRegister;
+import org.intalio.tempo.workflow.auth.AuthException;
+import org.intalio.tempo.workflow.task.xml.TaskXMLConstants;
+import org.intalio.tempo.workflow.tms.AccessDeniedException;
+import org.intalio.tempo.workflow.tms.TMSException;
+import org.intalio.tempo.workflow.tms.UnavailableAttachmentException;
+import org.intalio.tempo.workflow.tms.UnavailableTaskException;
+import org.intalio.tempo.workflow.tms.server.PIPAComponentManager;
+import org.intalio.tempo.workflow.tms.server.TMSConstants;
+import org.intalio.tempo.workflow.util.xml.InvalidInputFormatException;
+import org.intalio.tempo.workflow.util.xml.OMMarshaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class TMSRequestProcessor {   
+    final static Logger _logger = LoggerFactory.getLogger(TMSRequestProcessor.class);
+    private ITMSServer _server;
+    private static final OMFactory OM_FACTORY = OMAbstractFactory.getOMFactory();
+    
+    /**
+     * dumy function
+     * @return
+     */
+    public OMElement marshallResponse(){
+        return new TMSResponseMarshaller(OM_FACTORY) {
+            public OMElement createOkResponse() {
+                OMElement okResponse = createElement("okResponse");
+                return okResponse;
+            }
+        }.createOkResponse();
+    }
+    
+    public OMElement createOkResponse(){
+        return new TMSResponseMarshaller(OM_FACTORY) {
+            public OMElement createOkResponse() {
+                OMElement okResponse = createElement("okResponse");
+                return okResponse;
+            }
+        }.createOkResponse();
+    }
+    private AxisFault makeFault(Exception e) {
+        if (e instanceof TMSException) {
+            if (_logger.isDebugEnabled())
+                _logger.debug(e.getMessage(), e);
+            OMElement response = null;
+            if (e instanceof InvalidInputFormatException)
+                response = OM_FACTORY.createOMElement(TMSConstants.INVALID_INPUT_FORMAT);
+            else if (e instanceof AccessDeniedException)
+                response = OM_FACTORY.createOMElement(TMSConstants.ACCESS_DENIED);
+            else if (e instanceof UnavailableTaskException)
+                response = OM_FACTORY.createOMElement(TMSConstants.UNAVAILABLE_TASK);
+            else if (e instanceof UnavailableAttachmentException)
+                response = OM_FACTORY.createOMElement(TMSConstants.UNAVAILABLE_ATTACHMENT);
+            else if (e instanceof AuthException)
+                response = OM_FACTORY.createOMElement(TMSConstants.INVALID_TOKEN);
+
+            else
+                return AxisFault.makeFault(e);
+
+            response.setText(e.getMessage());
+            AxisFault axisFault = new AxisFault(e.getMessage(), e);
+            axisFault.setDetail(response);
+            return axisFault;
+        } else if (e instanceof AxisFault) {
+            _logger.error(e.getMessage(), e);
+            return (AxisFault) e;
+        } else {
+            _logger.error(e.getMessage(), e);
+            return AxisFault.makeFault(e);
+        }
+    }
+    public void setServer(ITMSServer server) {
+        _logger.debug("TMSRequestProcessor.setServer:" + server.getClass().getSimpleName());
+        _server = server;
+    }
+   
+    public OMElement create(OMElement requestElement) throws AxisFault{
+        try{
+        // unmarshall request
+        
+        
+        // process request
+      // _server.create(task, participantToken);
+        
+        // marshall response        
+        return createOkResponse();
+    } catch (Exception e) {
+        throw makeFault(e);
+    }
+    
+    }
     /**
      * @param args
      */
@@ -9,5 +102,9 @@ public class TMSRequestProcessor {
         // TODO Auto-generated method stub
 
     }
-
+    private abstract class TMSResponseMarshaller extends OMMarshaller {
+        public TMSResponseMarshaller(OMFactory omFactory) {
+            super(omFactory, omFactory.createOMNamespace("http://www.intalio.com/BPMS/Workflow/TaskManagementServices-20051109/", "tmsb4p"));
+        }
+    }
 }
--- tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSServer.java
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tmsb4p/server/TMSServer.java
@@ -1,13 +1,121 @@
 package org.intalio.tempo.workflow.tmsb4p.server;
 
-public class TMSServer {
+import java.net.URL;
 
+import org.apache.axis2.AxisFault;
+import org.intalio.tempo.workflow.auth.AuthIdentifierSet;
+import org.intalio.tempo.workflow.auth.UserRoles;
+import org.intalio.tempo.workflow.task.PIPATask;
+import org.intalio.tempo.workflow.task.Task;
+import org.intalio.tempo.workflow.task.TaskState;
+import org.intalio.tempo.workflow.task.attachments.Attachment;
+import org.intalio.tempo.workflow.tms.TMSException;
+
+import org.w3c.dom.Document;
+
+public class TMSServer implements ITMSServer{
+ 
     /**
      * @param args
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+    }
+
+    public void addAttachment(String taskID, Attachment attachment, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void complete(String taskID, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void create(Task task, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void delete(String[] taskIDs, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void deleteAll(boolean fakeDelete, String subquery, String subqueryClass, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void deletePipa(String formUrl, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void fail(String taskID, String failureCode, String failureReason, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Attachment[] getAttachments(String taskID, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Task[] getAvailableTasks(String participantToken, String taskType, String subQuery) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public PIPATask getPipa(String formUrl, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Task getTask(String taskID, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Task[] getTaskList(String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public UserRoles getUserRoles(String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Document initProcess(String taskID, Document input, String participantToken) throws TMSException, AxisFault {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void reassign(String taskID, AuthIdentifierSet users, AuthIdentifierSet roles, TaskState state, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void removeAttachment(String taskID, URL attachmentURL, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setOutput(String taskID, Document output, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setOutputAndComplete(String taskID, Document output, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void storePipa(PIPATask task, String participantToken) throws TMSException {
+        // TODO Auto-generated method stub
+        
     }
 
 }
