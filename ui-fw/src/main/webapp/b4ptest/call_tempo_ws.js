6125a4869005412e6215c28b02ae83e4bdbfa2cb
jackie <jackie@f54485bd-ff3a-0410-97a1-83db373c2bce>
2009-01-21 17:39:37 +0000

    add all operations
    git-svn-id: svn+ssh://www.intalio.org/usr/local/svn/repos/tempo/branches/b4p@1305 f54485bd-ff3a-0410-97a1-83db373c2bce

---
+++ ui-fw/src/main/webapp/b4ptest/call_tempo_ws.js
@@ -0,0 +1,64 @@
+
+
+
+    var service ;
+    var m;
+    var error;
+    function callws(method, p)
+    {
+        error=false;
+        m=method;
+    	//var url = "http://localhost:8080/axis2/services/TaskManagementServices";
+    	var url = "http://localhost:8080/axis2/services/HumanTaskOperationServices";
+        //设置webService传入参数
+        //
+        //注意:
+        //
+        //    调用webservice(如例子中的webservicedemo.asmx)
+        //           HelloTo(String name)   针对name参数必xu写成name=wqj ,还有更多参数一样写,使用&符号分隔(name=11&age=20&.....),使用名称匹配
+        //           传入的参数数量可以不等于(多于或少于)方法要求的参数
+     // alert(m);  
+        
+        var para = p;
+           
+        var op = {
+	            data:para,
+                    onComplete: showResponse,
+                    onFailure:showError,
+                    update:'ajaxBack'
+                 };
+
+        service = new WebService(url, method, op);
+      
+         service.request();
+       
+    //alert($("#"+method +" #input").size());
+   
+       $("#"+m+" #"+m+"_input" ).css("display", "none");
+       $("#"+m+" #"+m+"_output").empty().append("<div style='padding-top:100px;padding-bottom:100px;'><center><img src='ajax_load.gif'/></center></div>");
+       $("#"+m+" #"+m+"_output").css("display", "block");
+      
+        return false;
+    }
+    function showError(obj)
+    {
+		//obj 是一个xmlHttpRequest对象
+	//	alert("error="+obj.responseText+","+obj.statusText);
+		error= true;		
+		$("#"+m+" #"+m+"_output").empty().append("Response<br/><textArea id=outputtext  rows=13 cols=60>"+obj.responseText+"</textArea>");
+		
+    }
+    function showResponse(requestText,requestXML)
+    {
+		//requestText 返回的文本
+		//requestXML 返回的XML
+		//alert(requestText.responseText);
+		//alert(service.getBackData(requestXML));
+		    
+		if (error)
+		return;
+        $("#"+m+" #"+m+"_output").empty().append("Response<br/><pre><textArea id=outputtext rows=13 cols=55>"+requestText.responseText+"</textArea></pre>");
+    }
+
+
+//callws('getTaskList0', '<participantToken>LL</participantToken>');
