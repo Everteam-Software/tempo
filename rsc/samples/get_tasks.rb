108486e43c18a097d55666f21c47bd25d23ac7de
niko <niko@Lemonbook.local>
2009-03-06 14:37:05 +0900

    formatting and some minor corrections to the install instructions

--- rsc/samples/get_tasks.rb
+++ rsc/samples/get_tasks.rb
@@ -1,39 +1,5 @@
 #!/usr/bin/env ruby
-
-require 'soap/wsdlDriver'
-
-class SampleTMSClient 
-
-  attr_reader :token
-
-  def initialize(host="localhost:8080", user="admin",password="changeit")
-    @host = host
-    @token = authenticate(user,password)
-    @tms_client = SOAP::WSDLDriverFactory.new("http://#{@host}/axis2/services/TaskManagementServices?wsdl").create_rpc_driver if not @tms_client
-  end
-
-  def get_tasks
-    tasks = @tms_client.getTaskList(:participantToken => @token).task
-  end
-
-  def get_pa_tasks
-    tasks = @tms_client.getAvailableTasks(:participantToken => @token, :taskType => "PATask", :subQuery=>"").task
-  end
-  
-  def delete tasks
-    tasks.each do |task|
-      puts "Deleting task #{task.taskId}"
-      @tms_client.delete(:participantToken => @token, :taskId=> task.taskId)
-    end
-  end
-
-  private
-  def authenticate(user, password)
-    tokenService = SOAP::WSDLDriverFactory.new("http://#{@host}/axis2/services/TokenService?wsdl").create_rpc_driver    
-    @token = tokenService.authenticateUser(:user => user, :password => password).token
-  end
-
-end
+load "sample_tms_client"
 
 def display_tasks tasks
   puts "\tFound #{tasks.size} tasks"
