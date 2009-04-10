da0ff1c7c83ddc6ec8b4a94357465ee1c9f89b6f
release <release@intalio.com>
2009-04-10 13:54:55 -0500

    security 1.0.2

--- rsc/build/dependencies.rb
+++ rsc/build/dependencies.rb
@@ -304,8 +304,8 @@ CAS_LIBS = [
 
 DEPLOY_API = "org.intalio.deploy:deploy-api:jar:1.0.3"
 REGISTRY = "org.intalio.deploy:deploy-registry:jar:1.0.3"
-SECURITY = group("security-api", :under=>"org.intalio.security", :version=>"1.0.1")
-SECURITY_WS_COMMON = group("security-ws-common", :under=>"org.intalio.security", :version=>"1.0.1")
-SECURITY_WS_CLIENT = group("security-api", "security-ws-client", "security-ws-common", :under=>"org.intalio.security", :version=>"1.0.1")
-SECURITY_WS_CLIENT_ONLY = group("security-api", "security-ws-client", :under=>"org.intalio.security", :version=>"1.0.1")
-WEB_NUTSNBOLTS = "org.intalio.security:security-web-nutsNbolts:jar:1.0.1"
+SECURITY = group("security-api", :under=>"org.intalio.security", :version=>"1.0.2")
+SECURITY_WS_COMMON = group("security-ws-common", :under=>"org.intalio.security", :version=>"1.0.2")
+SECURITY_WS_CLIENT = group("security-api", "security-ws-client", "security-ws-common", :under=>"org.intalio.security", :version=>"1.0.2")
+SECURITY_WS_CLIENT_ONLY = group("security-api", "security-ws-client", :under=>"org.intalio.security", :version=>"1.0.2")
+WEB_NUTSNBOLTS = "org.intalio.security:security-web-nutsNbolts:jar:1.0.2"
