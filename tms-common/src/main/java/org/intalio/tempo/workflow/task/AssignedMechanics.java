589cec79b076e24712f3fa81709658642a430d54
pavageau <pavageau@intalio.com>
2009-07-01 20:33:08 +0200

    Pierre - Ihab diffs

---
+++ tms-common/src/main/java/org/intalio/tempo/workflow/task/AssignedMechanics.java
@@ -0,0 +1,33 @@
+package org.intalio.tempo.workflow.task;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.apache.openjpa.persistence.Persistent;
+
+@Entity
+@Table(name = "tempo_assignedMechanisB")
+public class AssignedMechanics {
+	@Persistent
+	@Column(name = "name")
+	String _name;
+	@Persistent
+	@Column(name = "mechanicID")
+	String _mechanicID;
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String _name) {
+		this._name = _name;
+	}
+
+	public String getMechanicID() {
+		return _mechanicID;
+	}
+
+	public void setMechanicID(String _mechanicid) {
+		_mechanicID = _mechanicid;
+	}
+}
