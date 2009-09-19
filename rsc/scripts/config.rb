5e1c5de08ba944cafe3066355bb11b63a4a83ad1
arkxu <ark.work@gmail.com>
2009-09-20 05:52:59 +0800

    add sample config for liferay 5_2_5ee and 5_2_3

--- rsc/scripts/config.rb
+++ rsc/scripts/config.rb
@@ -46,7 +46,8 @@ BUILD_CONFIG = {
   
   :ode => :v1_3_snapshot,
   :tomcat => :v5,
-  :liferay => {:v => :v5_2_3, :base_folder => "liferay-portal-5.2.3", :server_folder => "tomcat-5.5.27"},
+  :liferay => {:v => :v5_2_5ee, :base_folder => "liferay-portal-5.2.5", :server_folder => "tomcat-5.5.27"},
+  #:liferay => {:v => :v5_2_3, :base_folder => "liferay-portal-5.2.3", :server_folder => "tomcat-5.5.27"},
   :alfresco => :v3_0,
   :tempo => {
     :core => "6.0.0.47",
