515de1335c1e78938302b5b47e58709375a4733d
niko <niko@niko.local>
2009-10-16 14:53:02 +0900

    updated script for liferay release; added tomcat-ee

--- rsc/scripts/tempo_liferay_builder.rb
+++ rsc/scripts/tempo_liferay_builder.rb
@@ -5,14 +5,26 @@
 load "#{@@script_folder}/../scripts/tempo_builder_lib.rb"
 load "#{@@script_folder}/../scripts/config.rb"
 
+BASE_PACKAGE = "org.intalio.liferay"
+VERSION_NUMBER = '6.0.002'
+TEMPO_VERSION = "6.0.0.77-SNAPSHOT"
+
 BUILD_CONFIG = {
   :directory => "./target",
-  :mode => [BuildMode::LIFERAY, BuildMode::UIFW, BuildMode::CAS, BuildMode::RELEASE],
   # :liferay => {:v => :v5_2_5ee, :base_folder => "liferay-portal-5.2.5", :server_folder => "tomcat-5.5.27"},
   # :liferay => {:v => :v5_2_3, :base_folder => "liferay-portal-5.2.3", :server_folder => "tomcat-5.5.27"},
-  :liferay => {:v => :v5_2_5_jbee, :base_folder => "liferay-portal-5.2.5", :server_folder => "jboss-tomcat-4.2.3"},  
-  :tempo => {:core => "6.0.0.77-SNAPSHOT"},
-  :artifact => "org.intalio.liferay:liferay-jboss-ee:zip:6.0.001"
+  # :liferay => {:v => :v5_2_5_jbee, :base_folder => "liferay-portal-5.2.5", :server_folder => "jboss-tomcat-4.2.3"},
+  :mode => [BuildMode::LIFERAY, BuildMode::UIFW, BuildMode::CAS, BuildMode::RELEASE],
+  :tempo => {:core => TEMPO_VERSION},
+  # :artifact => "org.intalio.liferay:liferay-jboss-ee:zip:6.0.001"
 }
+
+BUILD_CONFIG[:artifact] = "${BASE_PACKAGE}:liferay-jboss-ee:zip:${VERSION_NUMBER}"
+BUILD_CONFIG[:liferay] = {:v => :v5_2_5_jbee, :base_folder => "liferay-portal-5.2.5", :server_folder => "jboss-tomcat-4.2.3"}
+tb = TempoBuilder.new
+tb.build BUILD_CONFIG
+
+BUILD_CONFIG[:artifact] = "${BASE_PACKAGE}:liferay-tomcat-ee:zip:${VERSION_NUMBER}"
+BUILD_CONFIG[:liferay] = {:v => :v5_2_5ee, :base_folder => "liferay-portal-5.2.5", :server_folder => "tomcat-5.5.27"}
 tb = TempoBuilder.new
 tb.build BUILD_CONFIG
