4b11f43a527b370b52e543274df660f88e925b2e
Nicolas Modrzyk <hellonico@gmail.com>
2010-02-04 16:35:46 +0900

    minor fixes for previous commit on TAS. Override throwing errors

--- tas-service/src/test/java/org/intalio/tempo/workflow/tas/live/FileDataSource.java
+++ tas-service/src/test/java/org/intalio/tempo/workflow/tas/live/FileDataSource.java
@@ -22,28 +22,24 @@ public class FileDataSource implements DataSource {
 	private FileInputStream fis;
 	private FileOutputStream fos;
 
-	@Override
+	
 	public String getContentType() {
-		// TODO Auto-generated method stub
 		return MimetypesFileTypeMap.getDefaultFileTypeMap()
 				.getContentType(file);
 	}
 
-	@Override
+	
 	public InputStream getInputStream() throws IOException {
-		// TODO Auto-generated method stub
 		return fis;
 	}
 
-	@Override
+	
 	public String getName() {
-		// TODO Auto-generated method stub
 		return file.getName();
 	}
 
-	@Override
+	
 	public OutputStream getOutputStream() throws IOException {
-		// TODO Auto-generated method stub
 		return fos;
 	}
 
