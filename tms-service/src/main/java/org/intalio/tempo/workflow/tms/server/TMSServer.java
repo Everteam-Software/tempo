8bebc1501d488794abadb576354d2e4a3be74c24
release <github@intalio.com>
2011-03-16 13:46:36 -0500

    Changed WS timeout value

--- tms-service/src/main/java/org/intalio/tempo/workflow/tms/server/TMSServer.java
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tms/server/TMSServer.java
@@ -65,7 +65,7 @@ public class TMSServer implements ITMSServer {
     private static final Logger _logger = LoggerFactory.getLogger(TMSServer.class);
     private IAuthProvider _authProvider;
     private TaskPermissions _permissions;
-    private int _httpTimeout = 10000;
+    private int _httpTimeout = 120000;
     private String _tasEndPoint;
     private static final String _tas_NameSpace= "http://www.intalio.com/BPMS/Workflow/TaskAttachmentService/";
    
