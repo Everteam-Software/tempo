f0683b948a58fba9af5d9c76732c2c8f6e8c0dbd
Bapiraju Akkina <bapiraju@intalio.com>
2013-06-14 14:03:12 +0530

    WF-1647 - Fixed vacations which ends today are not displayed in the list

--- tms-service/src/main/java/org/intalio/tempo/workflow/tms/server/dao/JPAVacationDAOConnection.java
+++ tms-service/src/main/java/org/intalio/tempo/workflow/tms/server/dao/JPAVacationDAOConnection.java
@@ -19,6 +19,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.TemporalType;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.intalio.tempo.workflow.dao.AbstractJPAConnection;
 import org.intalio.tempo.workflow.task.Vacation;
 import org.intalio.tempo.workflow.task.audit.VacationAudit;
@@ -34,13 +35,15 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
 		_vacation = new Vacation(createEntityManager);
 	}
 
-	public void insertVacationDetails(Vacation vacation) {
+	@Override
+	public final void insertVacationDetails(final Vacation vacation) {
 		vacation.setIs_active(1);
 		checkTransactionIsActive();
 		entityManager.persist(vacation);
 	}
-	
-	public void updateVacationDetails(Vacation vacation) {
+
+	@Override
+	public final void updateVacationDetails(final Vacation vacation) {
 		checkTransactionIsActive();
 		Vacation prevVacation = _vacation.fetchVacationByID(vacation.getId());
 		
@@ -54,24 +57,33 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
 		entityManager.persist(prevVacation);
 	}
 
-        /**
-         * get vacation details for selected user.
-         * @param user String
-         * @return vacation list List<Vacation>
-         */
-        public final List<Vacation> getVacationDetails(final String user) {
-            Query query = entityManager
-                    .createNamedQuery(Vacation.GET_VACATION_DETAILS, Vacation.class)
-                    .setParameter("user", user)
-                    .setParameter("toDate", Calendar.getInstance().getTime(),
-                            TemporalType.DATE);
-            List<Vacation> result = query.getResultList();
-            return result;
-        }
-	
-	public List<Vacation> getMatchedVacations(Date fromDate, Date toDate) {
-		Query query = entityManager.createNamedQuery(Vacation.FETCH_MATCHED_VACATION, Vacation.class).setParameter(
-				"fromDate", fromDate).setParameter("toDate", toDate);
+    /**
+     * get vacation details for selected user.
+     * @param user String
+     * @return vacation list List<Vacation>
+     */
+    public final List<Vacation> getVacationDetails(final String user) {
+        Calendar cal = Calendar.getInstance();
+        Query query = entityManager
+                .createNamedQuery(Vacation.GET_VACATION_DETAILS,
+                        Vacation.class)
+                .setParameter("user", user)
+                .setParameter("toDate", this.trimDate(cal.getTime()),
+                        TemporalType.DATE);
+        List<Vacation> result = query.getResultList();
+        return result;
+    }
+
+    @Override
+    public final List<Vacation> getMatchedVacations(final Date fromDate,
+            final Date toDate) {
+        Query query = entityManager
+                .createNamedQuery(Vacation.FETCH_MATCHED_VACATION,
+                        Vacation.class)
+                .setParameter("fromDate", this.trimDate(fromDate),
+                        TemporalType.DATE)
+                .setParameter("toDate", this.trimDate(toDate),
+                        TemporalType.DATE);
 		List<Vacation> result = query.getResultList();
 		return result;
 	}
@@ -81,14 +93,16 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
 	 * @return vacation list List<Vacation>
 	 */
 	public final List<Vacation> getVacationDetails() {
-        Query query = entityManager.createNamedQuery(
+	    Calendar cal = Calendar.getInstance();
+	    Query query = entityManager.createNamedQuery(
                 Vacation.FETCH_VACATION_SUMMARY, Vacation.class).setParameter(
-                "toDate", Calendar.getInstance().getTime(), TemporalType.DATE);
+                "toDate", this.trimDate(cal.getTime()), TemporalType.DATE);
 		List<Vacation> resultList = query.getResultList();
 		return resultList;
 	}
 
-	public Boolean deleteVacationDetails(int id) {
+	@Override
+	public final Boolean deleteVacationDetails(final int id) {
 		checkTransactionIsActive();
 		LOG.debug("vacation details=" + _vacation.fetchVacationByID(id));
 		Vacation prevVacation = _vacation.fetchVacationByID(id);
@@ -106,7 +120,7 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
     public final List<Vacation> getVacationsByStartDate(final Date fromDate) {
         Query query = entityManager.createNamedQuery(
                 Vacation.FETCH_START_VACATION, Vacation.class).setParameter(
-                "fromDate", fromDate, TemporalType.DATE);
+                "fromDate", this.trimDate(fromDate), TemporalType.DATE);
         List<Vacation> result = query.getResultList();
         return result;
     }
@@ -120,7 +134,7 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
     public final List<Vacation> getVacationsByEndDate(final Date toDate) {
         Query query = entityManager.createNamedQuery(
                 Vacation.FETCH_END_VACATION, Vacation.class).setParameter(
-                "toDate", toDate, TemporalType.DATE);
+                "toDate", this.trimDate(toDate), TemporalType.DATE);
         List<Vacation> result = query.getResultList();
         return result;
     }
@@ -142,8 +156,11 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
             final String substitute, final Date fromDate, final Date toDate) {
         Query query = entityManager
                 .createNamedQuery(Vacation.FETCH_SUBSTITUTE_MATCHED_VACATION,
-                        Vacation.class).setParameter("fromDate", fromDate)
-                .setParameter("toDate", toDate)
+                        Vacation.class)
+                .setParameter("fromDate", this.trimDate(fromDate),
+                        TemporalType.DATE)
+                .setParameter("toDate", this.trimDate(toDate),
+                        TemporalType.DATE)
                 .setParameter("user", substitute);
         List<Vacation> result = query.getResultList();
         return result;
@@ -159,5 +176,14 @@ public class JPAVacationDAOConnection extends AbstractJPAConnection implements V
     public final Vacation getVacationsByID(final int id) {
         checkTransactionIsActive();
         return _vacation.fetchVacationByID(id);
+    }
+
+    /**
+     * trim time from date object.
+     * @param date Date
+     * @return trimmedDate Date
+     */
+    private Date trimDate(final Date date) {
+        return DateUtils.truncate(date, Calendar.DATE);
     }
 }
