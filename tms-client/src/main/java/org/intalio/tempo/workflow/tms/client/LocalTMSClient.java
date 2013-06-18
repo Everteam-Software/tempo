e72b8852c0e65af17666773484c76f2f4ac8f348
Himanshu Mishra <mishra@intalio.com>
2013-06-18 15:31:27 +0530

    Implemented to fetchAvailableTask from RemoteTMSClient without meta data
    Conflicts:
    	tms-client/src/main/java/org/intalio/tempo/workflow/tms/client/LocalTMSClient.java

--- tms-client/src/main/java/org/intalio/tempo/workflow/tms/client/LocalTMSClient.java
+++ tms-client/src/main/java/org/intalio/tempo/workflow/tms/client/LocalTMSClient.java
@@ -462,4 +462,12 @@ public class LocalTMSClient implements ITaskManagementService {
         }
         return vac;
     }
+
+    @Override
+    public Task[] getAvailableTasks(String taskType, String subQuery,
+            String first, String max, String fetchMetaData)
+            throws AuthException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
