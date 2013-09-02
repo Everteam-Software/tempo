1d30d28896c348bbe70698860405ba6ac35a788e
Veresh Jain <veresh@intalio.com>
2013-09-02 15:53:54 +0530

    Added password

--- rsc/build/repositories.rb
+++ rsc/build/repositories.rb
@@ -8,5 +8,6 @@ repositories.remote = [
 ]
 
 repositories.release_to[:username] ||= "release"
+repositories.release_to[:password] ||= "intalio"
 repositories.release_to[:url] ||= "sftp://www.intalio.org/var/www-org/public/maven2"
 repositories.release_to[:permissions] ||= 0664
