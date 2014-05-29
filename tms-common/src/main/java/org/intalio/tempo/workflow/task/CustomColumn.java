effe3c0bb049c308629dee3eddb236998103bcfb
Bapiraju Akkina <bapiraju@intalio.com>
2014-05-29 18:25:17 +0530

    Fixed QueryParameterException in Custom Column

--- tms-common/src/main/java/org/intalio/tempo/workflow/task/CustomColumn.java
+++ tms-common/src/main/java/org/intalio/tempo/workflow/task/CustomColumn.java
@@ -16,7 +16,7 @@ import javax.persistence.TableGenerator;
 @Entity
 @Table(name="tempo_custom_column")
 @NamedQueries({
-    @NamedQuery(name = CustomColumn.FIND_BY_PROCESS_NAME, query = "select distinct t from CustomColumn t where t._projectName=(?1)"),
+    @NamedQuery(name = CustomColumn.FIND_BY_PROCESS_NAME, query = "select distinct t from CustomColumn t where t._projectName=(?)"),
     @NamedQuery(name=CustomColumn.FIND_ALL_CUSTOM_COLUMNS, query="select distinct t.customColumnName from CustomColumn t" )}
 )
 public class CustomColumn {
